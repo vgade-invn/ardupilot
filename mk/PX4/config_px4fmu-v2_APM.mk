4e0bf04832e71ec8614693d66105443049c77126
Andrew Tridgell <andrew@tridgell.net>
2016-07-15 19:13:52 +1000

    PX4: disable uavcan for reference build

--- mk/PX4/config_px4fmu-v2_APM.mk
+++ mk/PX4/config_px4fmu-v2_APM.mk
@@ -8,5 +8,5 @@ MODULES		+= drivers/l3gd20
 MODULES		+= drivers/mpu9250
 MODULES		+= drivers/boards/px4fmu-v2
 MODULES		+= drivers/pwm_input
-MODULES         += modules/uavcan
+#MODULES         += modules/uavcan
 MODULES         += lib/mathlib
