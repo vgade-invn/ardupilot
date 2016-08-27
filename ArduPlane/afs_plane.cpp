98aa8cdd69f40233ef322ff0ed7090504f131919
Andrew Tridgell <andrew@tridgell.net>
2016-08-27 12:11:16 +1000

    Plane: disarm on AFS termination

--- ArduPlane/afs_plane.cpp
+++ ArduPlane/afs_plane.cpp
@@ -43,6 +43,9 @@ void AP_AdvancedFailsafe_Plane::terminate_vehicle(void)
     ch_yaw->output();
     ch_throttle->output();
     RC_Channel_aux::output_ch_all();
+
+    // also disarm to ensure that ignition is cut
+    plane.disarm_motors();
 }
 
 void AP_AdvancedFailsafe_Plane::setup_IO_failsafe(void)
