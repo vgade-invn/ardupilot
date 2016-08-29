77e91725ed04434211bfac91bc996d1b778479dd
Andrew Tridgell <andrew@tridgell.net>
2016-08-29 18:04:42 +1000

    Plane: added auto fence enable on VTOL takeoff
    fixes issue#724

--- ArduPlane/quadplane.cpp
+++ ArduPlane/quadplane.cpp
@@ -1693,6 +1693,9 @@ bool QuadPlane::verify_vtol_takeoff(const AP_Mission::Mission_Command &cmd)
     }
     transition_state = TRANSITION_AIRSPEED_WAIT;
     plane.TECS_controller.set_pitch_max_limit(transition_pitch_max);
+
+    plane.complete_auto_takeoff();
+    
     return true;
 }
 
