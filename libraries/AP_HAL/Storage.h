d052e0884be0683b8548199dec1477ddfc9067d5
Andrew Tridgell <andrew@tridgell.net>
2016-09-24 16:20:02 +1000

    AP_HAL: added optional sync() and reopen()
    for FactoryTest

--- libraries/AP_HAL/Storage.h
+++ libraries/AP_HAL/Storage.h
@@ -8,4 +8,10 @@ public:
     virtual void init() = 0;
     virtual void read_block(void *dst, uint16_t src, size_t n) = 0;
     virtual void write_block(uint16_t dst, const void* src, size_t n) = 0;
+
+    // block until all data is on stable storage. Used by FactoryTest
+    virtual bool sync(void) { return false; }
+
+    // discard all cached data and re-open storage. Used by FactoryTest
+    virtual bool reopen(void) { return false; }
 };
