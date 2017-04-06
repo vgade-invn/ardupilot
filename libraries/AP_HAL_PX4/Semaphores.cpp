c90e791241506670721148512a9fd2128b1196ff
Andrew Tridgell <andrew@tridgell.net>
2017-04-07 09:19:07 +1000

    HAL_PX4: WIP timed semaphores

--- libraries/AP_HAL_PX4/Semaphores.cpp
+++ libraries/AP_HAL_PX4/Semaphores.cpp
@@ -9,36 +9,42 @@ extern const AP_HAL::HAL& hal;
 
 using namespace PX4;
 
+Semaphore::Semaphore(void)
+{
+    printf("**** SEM init %p\n", &sem);
+    sem_init(&sem, 0, 0);
+}
+
 bool Semaphore::give() 
 {
-    return pthread_mutex_unlock(&_lock) == 0;
+    printf("post %p\n", &sem);
+    return sem_post(&sem) == OK;
 }
 
 bool Semaphore::take(uint32_t timeout_ms) 
 {
     if (up_interrupt_context()) {
         // don't ever wait on a semaphore in interrupt context
-        return take_nonblocking();
+        printf("irq wait %p %u\n", &sem, timeout_ms);
+        return sem_trywait(&sem) == OK;
     }
     if (timeout_ms == 0) {
-        return pthread_mutex_lock(&_lock) == 0;
+        // no timeout
+        printf("nowait %p %u\n", &sem, timeout_ms);
+        bool ret = (sem_wait(&sem) == OK);
+        printf("nowait done %p %u %d\n", &sem, timeout_ms, ret);
+        return ret;
     }
-    if (take_nonblocking()) {
-        return true;
-    }
-    uint64_t start = AP_HAL::micros64();
-    do {
-        hal.scheduler->delay_microseconds(200);
-        if (take_nonblocking()) {
-            return true;
-        }
-    } while ((AP_HAL::micros64() - start) < timeout_ms*1000);
-    return false;
+    struct timespec ts {};
+    ts.tv_sec = timeout_ms / 1000;
+    ts.tv_nsec = (timeout_ms % 1000) * 1000UL * 1000UL;
+    printf("wait %p %u\n", &sem, timeout_ms);
+    return sem_timedwait(&sem, &ts) == OK;
 }
 
 bool Semaphore::take_nonblocking() 
 {
-    return pthread_mutex_trylock(&_lock) == 0;
+    return sem_trywait(&sem) == OK;
 }
 
 #endif // CONFIG_HAL_BOARD
