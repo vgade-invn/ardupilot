7ca8ea5aff458ee67d7e14029648bdafb5755cbd
Andrew Tridgell <andrew@tridgell.net>
2017-08-23 12:51:41 +1000

    HAL_Linux: register mpu9250 on SPI for MR100

--- libraries/AP_HAL_Linux/SPIDevice.cpp
+++ libraries/AP_HAL_Linux/SPIDevice.cpp
@@ -125,6 +125,10 @@ SPIDesc SPIDeviceManager::_device[] = {
     SPIDesc("aeroio", 1, 1, SPI_MODE_0, 8, SPI_CS_KERNEL,  10*MHZ, 10*MHZ),
     SPIDesc("bmi160", 3, 0, SPI_MODE_3, 8, SPI_CS_KERNEL, 1*MHZ, 10*MHZ),
 };
+#elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_MR100
+SPIDesc SPIDeviceManager::_device[] = {
+    SPIDesc("mpu9250", 0, 1, SPI_MODE_0, 8, 36,  10*MHZ, 10*MHZ),
+};
 #else
 // empty device table
 SPIDesc SPIDeviceManager::_device[] = {
