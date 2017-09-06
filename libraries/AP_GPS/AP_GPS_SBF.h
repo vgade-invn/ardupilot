c61efe2ac8bd7bc05ad695339f45943cae5c0b7c
Andrew Tridgell <andrew@tridgell.net>
2017-09-07 09:02:46 +1000

    AP_GPS: SBF: Reject short packets early, allow 256 byte long messages to be decoded

--- libraries/AP_GPS/AP_GPS_SBF.h
+++ libraries/AP_GPS/AP_GPS_SBF.h
@@ -130,7 +130,8 @@ private:
         msg4007 msg4007u;
         msg4001 msg4001u;
         msg4014 msg4014u;
-        uint8_t bytes[128];
+        msg5908 msg5908u;
+        uint8_t bytes[256];
     };
 
     struct sbf_msg_parser_t
