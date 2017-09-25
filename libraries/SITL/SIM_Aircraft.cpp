611393ab25035bbffa2df8d8bf45654d08fb4fd8
Andrew Tridgell <andrew@tridgell.net>
2017-09-25 11:11:46 +1000

    SITL: fixed simple SITL sketches
    _sitl is not set

--- libraries/SITL/SIM_Aircraft.cpp
+++ libraries/SITL/SIM_Aircraft.cpp
@@ -126,7 +126,7 @@ bool Aircraft::parse_home(const char *home_str, Location &loc, float &yaw_degree
 float Aircraft::ground_height_difference() const
 {
     float h1, h2;
-    if (sitl->terrain_enable && terrain &&
+    if (sitl && sitl->terrain_enable && terrain &&
         terrain->height_amsl(home, h1, false) &&
         terrain->height_amsl(location, h2, false)) {
         return h2 - h1;
@@ -190,19 +190,21 @@ void Aircraft::update_mag_field_bf()
     // calculate frame height above ground
     const float frame_height_agl = fmaxf((-position.z) + home.alt * 0.01f - ground_level, 0.0f);
 
-    // calculate scaling factor that varies from 1 at ground level to 1/8 at sitl->mag_anomaly_hgt
-    // Assume magnetic anomaly strength scales with 1/R**3
-    float anomaly_scaler = (sitl->mag_anomaly_hgt / (frame_height_agl + sitl->mag_anomaly_hgt));
-    anomaly_scaler = anomaly_scaler * anomaly_scaler * anomaly_scaler;
+    if (sitl) {
+        // calculate scaling factor that varies from 1 at ground level to 1/8 at sitl->mag_anomaly_hgt
+        // Assume magnetic anomaly strength scales with 1/R**3
+        float anomaly_scaler = (sitl->mag_anomaly_hgt / (frame_height_agl + sitl->mag_anomaly_hgt));
+        anomaly_scaler = anomaly_scaler * anomaly_scaler * anomaly_scaler;
+        
+        // add scaled anomaly to earth field
+        mag_ef += sitl->mag_anomaly_ned.get() * anomaly_scaler;
 
-    // add scaled anomaly to earth field
-    mag_ef += sitl->mag_anomaly_ned.get() * anomaly_scaler;
+        // Rotate into body frame
+        mag_bf = dcm.transposed() * mag_ef;
 
-    // Rotate into body frame
-    mag_bf = dcm.transposed() * mag_ef;
-
-    // add motor interference
-    mag_bf += sitl->mag_mot.get() * battery_current;
+        // add motor interference
+        mag_bf += sitl->mag_mot.get() * battery_current;
+    }
 }
 
 /* advance time by deltat in seconds */
