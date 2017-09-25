3f5cb715e8464ae12e6cb0bf791c4508c6b66807
Andrew Tridgell <andrew@tridgell.net>
2017-09-25 11:22:43 +1000

    AP_FlashStorage: enable test with stm32 flash

--- libraries/AP_FlashStorage/examples/FlashTest/FlashTest.cpp
+++ libraries/AP_FlashStorage/examples/FlashTest/FlashTest.cpp
@@ -4,8 +4,18 @@
 
 #include <AP_HAL/AP_HAL.h>
 #include <AP_Math/AP_Math.h>
+
+//#pragma GCC optimize("O0")
+
 #include <AP_FlashStorage/AP_FlashStorage.h>
 #include <stdio.h>
+
+#define USE_HAL_PX4_FLASH 1
+
+#if USE_HAL_PX4_FLASH
+#include <nuttx/progmem.h>
+static const uint32_t _flash_page = 22;
+#endif
 
 const AP_HAL::HAL& hal = AP_HAL::get_HAL();
 
@@ -16,13 +26,15 @@ public:
     void loop() override;
 
 private:
-    static const uint32_t flash_sector_size = 32U * 1024U;
+    static const uint32_t flash_sector_size = 128U * 1024U;
 
     uint8_t mem_buffer[AP_FlashStorage::storage_size];
     uint8_t mem_mirror[AP_FlashStorage::storage_size];
 
+#if !USE_HAL_PX4_FLASH
     // flash buffer
     uint8_t flash[2][flash_sector_size];
+#endif
 
     bool flash_write(uint8_t sector, uint32_t offset, const uint8_t *data, uint16_t length);
     bool flash_read(uint8_t sector, uint32_t offset, uint8_t *data, uint16_t length);
@@ -40,7 +52,80 @@ private:
     void write(uint16_t offset, const uint8_t *data, uint16_t length);
 
     bool erase_ok;
+    uint32_t _offset=0;
+    uint32_t _count=0;
 };
+
+#if USE_HAL_PX4_FLASH
+
+#define SETLINE do { _flash_line=__LINE__; _flash_line2=__LINE__; } while (0)
+
+static uint32_t _flash_line;
+static uint32_t _flash_offset;
+static uint32_t _flash_sector;
+static const uint8_t *_flash_data;
+static uint32_t _flash_length;
+static uint32_t _flash_line2;
+
+
+/*
+  callback to write data to flash
+ */
+bool FlashTest::flash_write(uint8_t sector, uint32_t offset, const uint8_t *data, uint16_t length)
+{
+    SETLINE;
+    _flash_sector = sector;
+    _flash_offset = offset;
+    _flash_data = data;
+    _flash_length = length;
+    
+    size_t base_address = up_progmem_getaddress(_flash_page+sector);
+    const uint8_t *b = ((const uint8_t *)base_address)+offset;
+    for (uint32_t i=0; i<length; i++) {
+        if (data[i] & ~b[i]) {
+            AP_HAL::panic("invalid flash write\n");
+            SETLINE;
+            return false;
+        }
+    }
+    SETLINE;
+    bool ret = up_progmem_write(base_address+offset, data, length) == length;
+    SETLINE;
+    if (memcmp(b, data, length) != 0) {
+        AP_HAL::panic("flash write failed\n");
+        SETLINE;
+        return false;
+    }
+    SETLINE;
+    return ret;
+}
+
+/*
+  callback to read data from flash
+ */
+bool FlashTest::flash_read(uint8_t sector, uint32_t offset, uint8_t *data, uint16_t length)
+{
+    SETLINE;
+    size_t base_address = up_progmem_getaddress(_flash_page+sector);
+    SETLINE;
+    const uint8_t *b = ((const uint8_t *)base_address)+offset;
+    SETLINE;
+    memcpy(data, b, length);
+    SETLINE;
+    return true;
+}
+
+/*
+  callback to erase flash sector
+ */
+bool FlashTest::flash_erase(uint8_t sector)
+{
+    SETLINE;
+    bool ret = up_progmem_erasepage(_flash_page+sector) > 0;
+    SETLINE;
+    return ret;
+}
+#else
 
 bool FlashTest::flash_write(uint8_t sector, uint32_t offset, const uint8_t *data, uint16_t length)
 {
@@ -54,6 +139,12 @@ bool FlashTest::flash_write(uint8_t sector, uint32_t offset, const uint8_t *data
                       (unsigned)length);
     }
     uint8_t *b = &flash[sector][offset];
+    for (uint32_t i=0; i<length; i++) {
+        if (data[i] & ~b[i]) {
+            AP_HAL::panic("invalid flash write\n");
+            return false;
+        }
+    }
     for (uint16_t i=0; i<length; i++) {
         b[i] &= data[i];
     }
@@ -84,6 +175,8 @@ bool FlashTest::flash_erase(uint8_t sector)
     return true;
 }
 
+#endif // USE_HAL_PX4_FLASH
+
 bool FlashTest::flash_erase_ok(void)
 {
     return erase_ok;
@@ -113,6 +206,7 @@ void FlashTest::setup(void)
         AP_HAL::panic("Failed first init()");
     }
 
+#if 0
     // fill with 10k random writes
     for (uint32_t i=0; i<5000000; i++) {
         uint16_t ofs = get_random16() % sizeof(mem_buffer);
@@ -153,11 +247,21 @@ void FlashTest::setup(void)
         AP_HAL::panic("FATAL: data mis-match");
     }
     AP_HAL::panic("TEST PASSED");
+#endif
 }
 
 void FlashTest::loop(void)
 {
-    hal.console->printf("loop\n");
+    erase_ok = true;
+    if (_count % 4096 == 0) {
+        printf("write %u at %u\n", (unsigned)_count, (unsigned)_offset);
+    }
+    for (uint32_t i=0; i<256; i++) {
+        write(_offset, (const uint8_t *)&_count, sizeof(_count));
+        _offset = (_offset + 4) % HAL_STORAGE_SIZE;
+        _count++;
+    }
+    hal.scheduler->delay(10);
 }
 
 FlashTest flashtest;
