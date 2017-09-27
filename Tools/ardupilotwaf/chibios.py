be64c0f81c8e33fa45595de8903e62d250357ef5
Andrew Tridgell <andrew@tridgell.net>
2017-09-27 15:34:50 +1000

    waf: don't use nodefaultlibs for chibios
    needed to allow for vsnprintf()

--- Tools/ardupilotwaf/chibios.py
+++ Tools/ardupilotwaf/chibios.py
@@ -140,7 +140,7 @@ def build(bld):
     bld.env.LIB += ['ch']
     bld.env.LIBPATH += ['modules/ChibiOS/']
     bld.env.LINKFLAGS += ['--specs=nano.specs']
-    wraplist = ['malloc', 'calloc', 'free', 'memalign', 'realloc']
+    wraplist = ['malloc', 'calloc', 'free', 'memalign', 'realloc', 'asprintf', 'vasprintf', 'vsnprintf', 'strdup', 'strndup']
     for w in wraplist:
         bld.env.LINKFLAGS += ['-Wl,--wrap,%s' % w]
     
