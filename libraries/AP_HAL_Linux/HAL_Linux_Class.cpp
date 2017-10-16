fee3c74b7576c0c9013dc58350ff7fa86a977f44
Andrew Tridgell <andrew@tridgell.net>
2017-10-16 16:14:47 +1100

    HAL_Linux: enable flow on MR100

--- libraries/AP_HAL_Linux/HAL_Linux_Class.cpp
+++ libraries/AP_HAL_Linux/HAL_Linux_Class.cpp
@@ -228,7 +228,8 @@ static Empty::RCOutput rcoutDriver;
 static Scheduler schedulerInstance;
 
 #if CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BEBOP ||\
-    CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_MINLURE
+    CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_MINLURE || \
+    CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_MR100
 static OpticalFlow_Onboard opticalFlow;
 #else
 static Empty::OpticalFlow opticalFlow;
