269185a83a97110fbb497a1be80bdfd4f23c97f4
Andrew Tridgell <andrew@tridgell.net>
2017-11-10 15:44:19 +1100

    Tools: added support for apj and abin files in apj_tool.py

--- Tools/scripts/apj_tool.py
+++ Tools/scripts/apj_tool.py
@@ -3,20 +3,90 @@
 tool to manupulate ArduPilot firmware files, changing default parameters
 '''
 
-import os, sys, struct
+import os, sys, struct, json, base64, zlib, hashlib
 
 import argparse
 
 class embedded_defaults(object):
+    '''class to manipulate embedded defaults in a firmware'''
     def __init__(self, filename):
         self.filename = filename
-        f = open(args.input_file,'r')
+        self.offset = 0
+        self.extension = os.path.splitext(filename)[1]
+        if self.extension.lower() in ['.apj', '.px4']:
+            self.load_apj()
+        elif self.extension.lower() in ['.abin']:
+            self.load_abin()
+        else:
+            self.load_binary()
+
+    def load_binary(self):
+        '''load firmware from binary file'''
+        f = open(self.filename,'r')
         self.firmware = f.read()
         f.close()
-        self.offset = 0
+        print("Loaded binary file of length %u" % len(self.firmware))
+
+    def load_abin(self):
+        '''load firmware from abin file'''
+        f = open(self.filename,'r')
+        self.headers = []
+        while True:
+            line = f.readline().rstrip()
+            if line == '--':
+                break
+            self.headers.append(line)
+            if len(self.headers) > 50:
+                print("Error: too many abin headers")
+                sys.exit(1)
+        self.firmware = f.read()
+        f.close()
+        print("Loaded abin file of length %u" % len(self.firmware))
+
+    def load_apj(self):
+        '''load firmware from a json apj or px4 file'''
+        f = open(self.filename,'r')
+        self.fw_json = json.load(f)
+        f.close()
+        image = self.fw_json['image']
+        self.firmware = zlib.decompress(base64.b64decode(self.fw_json['image']))
+        print("Loaded apj file of length %u" % len(self.firmware))
+
+    def save_binary(self):
+        '''save binary file'''
+        f = open(self.filename, 'w')
+        f.write(self.firmware)
+        f.close()
+        print("Saved binary of length %u" % len(self.firmware))
+
+    def save_apj(self):
+        '''save apj file'''
+        self.fw_json['image'] = base64.b64encode(zlib.compress(self.firmware, 9))
+        f = open(self.filename,'w')
+        json.dump(self.fw_json,f,indent=4)
+        f.truncate()
+        f.close()
+        print("Saved apj of length %u" % len(self.firmware))
+
+    def save_abin(self):
+        '''save abin file'''
+        f = open(self.filename,'w')
+        for i in range(len(self.headers)):
+            line = self.headers[i]
+            if line.startswith('MD5: '):
+                h = hashlib.new('md5')
+                h.update(self.firmware)
+                f.write('MD5: %s\n' % h.hexdigest())
+            else:
+                f.write(line+'\n')
+        f.write('--\n')
+        f.write(self.firmware)
+        f.close()
+        print("Saved abin of length %u" % len(self.firmware))
 
     def find(self):
         '''find defaults in firmware'''
+        # these are the magic headers from AP_Param.cpp
         magic_str = "PARMDEF"
         param_magic = [ 0x55, 0x37, 0xf4, 0xa0, 0x38, 0x5d, 0x48, 0x5b ]
         while True:
@@ -41,6 +111,7 @@ class embedded_defaults(object):
 
     def set_file(self, filename):
         '''set defaults to contents of a file'''
+        print("Setting defaults from %s" % filename)
         f = open(filename, 'r')
         contents = f.read()
         f.close()
@@ -56,9 +127,12 @@ class embedded_defaults(object):
 
     def save(self):
         '''save new firmware'''
-        f = open(self.filename, 'w')
-        f.write(self.firmware)
-        f.close()
+        if self.extension.lower() in ['.apj', '.px4']:
+            self.save_apj()
+        elif self.extension.lower() in ['.abin']:
+            self.save_abin()
+        else:
+            self.save_binary()
 
 def defaults_contents(firmware, ofs, length):
     '''return current defaults contents'''
@@ -74,10 +148,8 @@ args = parser.parse_args()
 
 defaults = embedded_defaults(args.input_file)
 
-print("Loaded firmware of length %u" % len(defaults.firmware))
-
 if not defaults.find():
-    print("Param defaults not found in firmware")
+    print("Error: Param defaults support not found in firmware")
     sys.exit(1)
     
 print("Found param defaults max_length=%u length=%u" % (defaults.max_len, defaults.length))
