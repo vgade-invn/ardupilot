26d54181ee78d3954debf3c7abbbc1a9d954fadd
Andrew Tridgell <andrew@tridgell.net>
2017-12-27 18:23:54 +1100

    HAL_ChibiOS: implement safety on/off with IOMCU

--- libraries/AP_HAL_ChibiOS/Util.cpp
+++ libraries/AP_HAL_ChibiOS/Util.cpp
@@ -3,6 +3,11 @@
 #include "Util.h"
 #include <chheap.h>
 
+#if HAL_WITH_IO_MCU
+#include <AP_IOMCU/AP_IOMCU.h>
+extern AP_IOMCU iomcu;
+#endif
+
 using namespace ChibiOS;
 /**
    how much free memory do we have in bytes.
@@ -15,7 +20,20 @@ uint32_t ChibiUtil::available_memory(void)
 
     // we also need to add in memory that is not yet allocated to the heap
     totalp += chCoreGetStatusX();
-    
+
     return totalp;
 }
+
+/*
+  get safety switch state
+ */
+ChibiUtil::safety_state ChibiUtil::safety_switch_state(void)
+{
+#if HAL_WITH_IO_MCU
+    return iomcu.get_safety_switch_state();
+#else
+    return SAFETY_NONE;
+#endif
+}
+    
 #endif //CONFIG_HAL_BOARD == HAL_BOARD_CHIBIOS
