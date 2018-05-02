58dd7b73fc059e53e41ba127d7090efb6adbd136
Andrew Tridgell <andrew@tridgell.net>
2018-05-02 13:22:13 +1000

    GCS_MAVLink: added ESC_TELEMETRY message

--- libraries/GCS_MAVLink/GCS_Common.cpp
+++ libraries/GCS_MAVLink/GCS_Common.cpp
@@ -20,6 +20,7 @@
 #include <AP_Vehicle/AP_Vehicle.h>
 #include <AP_RangeFinder/RangeFinder_Backend.h>
 #include <AP_Airspeed/AP_Airspeed.h>
+#include <AP_BLHeli/AP_BLHeli.h>
 
 #include "GCS.h"
 
@@ -2970,6 +2971,17 @@ bool GCS_MAVLINK::try_send_message(const enum ap_message id)
         CHECK_PAYLOAD_SIZE(VIBRATION);
         send_vibration();
         break;
+
+#if HAL_SUPPORT_RCOUT_SERIAL
+    case MSG_ESC_TELEMETRY: {
+        CHECK_PAYLOAD_SIZE(ESC_TELEMETRY_1_TO_4);
+        AP_BLHeli *blheli = AP_BLHeli::get_instance();
+        if (blheli) {
+            blheli->send_esc_telemetry_mavlink(uint8_t(chan));
+        }
+        break;
+    }
+#endif
 
     default:
         // try_send_message must always at some stage return true for
