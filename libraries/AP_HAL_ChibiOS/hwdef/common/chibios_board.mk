f896607236c5024d231d3a98d4978bc81f5d43aa
Andrew Tridgell <andrew@tridgell.net>
2018-06-02 13:27:02 +1000

    HAL_ChibiOS: switch to new bouncebuffer system
    this removes the dma_flush and dma_invalidate methods and uses a
    common bouncebuffer system for all CPU types. This enables microSD
    support on STM32F7

--- libraries/AP_HAL_ChibiOS/hwdef/common/chibios_board.mk
+++ libraries/AP_HAL_ChibiOS/hwdef/common/chibios_board.mk
@@ -121,7 +121,8 @@ CSRC += $(HWDEF)/common/stubs.c \
 	   $(HWDEF)/common/malloc.c \
 	   $(HWDEF)/common/stdio.c \
 	   $(HWDEF)/common/hrt.c \
-	   $(HWDEF)/common/stm32_util.c
+	   $(HWDEF)/common/stm32_util.c \
+	   $(HWDEF)/common/bouncebuffer.c
 
 ifeq ($(USE_FATFS),yes)
 CSRC += $(HWDEF)/common/posix.c
