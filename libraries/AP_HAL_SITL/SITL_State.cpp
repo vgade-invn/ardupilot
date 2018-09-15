ae28f47d533cb114a5b13f5f0428548e5f13d464
Andrew Tridgell <andrew@tridgell.net>
2018-09-16 08:43:29 +1000

    HAL_SITL: setup rc7 as 1500 for ICE

--- libraries/AP_HAL_SITL/SITL_State.cpp
+++ libraries/AP_HAL_SITL/SITL_State.cpp
@@ -501,6 +501,7 @@ void SITL_State::init(int argc, char * const argv[])
     pwm_input[0] = pwm_input[1] = pwm_input[3] = 1500;
     pwm_input[4] = pwm_input[7] = 1800;
     pwm_input[2] = pwm_input[5] = pwm_input[6] = 1000;
+    pwm_input[6] = 1500; // ICE control
 
     _scheduler = Scheduler::from(hal.scheduler);
     _parse_command_line(argc, argv);
