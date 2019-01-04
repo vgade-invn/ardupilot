842c719c241d6bc4cc9e97e734085b817201ab9e
Andrew Tridgell <andrew@tridgell.net>
2019-01-04 15:51:57 +1100

    AP_HAL: added IMUF IMU type for HelioSpring

--- libraries/AP_HAL/AP_HAL_Boards.h
+++ libraries/AP_HAL/AP_HAL_Boards.h
@@ -96,6 +96,7 @@
 #define HAL_INS_RST         21
 #define HAL_INS_LSM9DS1     22
 #define HAL_INS_ICM20789_SPI 23
+#define HAL_INS_IMUF_SPI    24
 
 /* Barometer driver types */
 #define HAL_BARO_NONE        0
