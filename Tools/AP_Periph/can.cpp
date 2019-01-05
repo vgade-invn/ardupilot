ff55c909563529c07200f0cfafab1c37bd75d2b9
Andrew Tridgell <andrew@tridgell.net>
2019-01-05 16:11:49 +1100

    AP_Periph: use generated headers for GetNodeInfo

--- Tools/AP_Periph/can.cpp
+++ Tools/AP_Periph/can.cpp
@@ -6,14 +6,11 @@
 #include <uavcan/protocol/dynamic_node_id/Allocation.h>
 #include <uavcan/protocol/NodeStatus.h>
 #include <uavcan/protocol/RestartNode.h>
+#include <uavcan/protocol/GetNodeInfo.h>
+#include <uavcan/protocol/file/BeginFirmwareUpdate.h>
 #include <ardupilot/bus/I2CReqAnnounce.h>
 #include <ardupilot/bus/I2CAnnounce.h>
 #include <ardupilot/bus/I2C.h>
-
-// we don't use the generated code for these ones as it doesn't
-// build as C++ due to void* cast
-//#include <uavcan/protocol/GetNodeInfo.h>
-//#include <uavcan/protocol/file/BeginFirmwareUpdate.h>
 
 extern const AP_HAL::HAL &hal;
 
@@ -56,17 +53,6 @@ static const CANConfig cancfg = {
 #define APP_VERSION_MINOR                                           0
 #define APP_NODE_NAME                                               "org.ardupilot.ap_periph"
 
-#define UAVCAN_GET_NODE_INFO_RESPONSE_MAX_SIZE                      ((3015 + 7) / 8)
-#define UAVCAN_GET_NODE_INFO_DATA_TYPE_SIGNATURE                    0xee468a8121c46a9e
-#define UAVCAN_GET_NODE_INFO_DATA_TYPE_ID                           1
-
-#define UAVCAN_PROTOCOL_FILE_BEGINFIRMWAREUPDATE_ID        40
-#define UAVCAN_PROTOCOL_FILE_BEGINFIRMWAREUPDATE_NAME      "uavcan.protocol.file.BeginFirmwareUpdate"
-#define UAVCAN_PROTOCOL_FILE_BEGINFIRMWAREUPDATE_SIGNATURE (0xB7D725DF72724126ULL)
-#define UAVCAN_PROTOCOL_FILE_BEGINFIRMWAREUPDATE_REQUEST_MAX_SIZE ((1616 + 7)/8)
-
-#define UNIQUE_ID_LENGTH_BYTES                                      16
-
 /*
  * Variables used for dynamic node ID allocation.
  * RTFM at http://uavcan.org/Specification/6._Application_level_functions/#dynamic-node-id-allocation
@@ -77,8 +63,7 @@ static uint8_t node_id_allocation_unique_id_offset;         ///< Depends on the 
 /*
  * Node status variables
  */
-static uint8_t node_health = UAVCAN_PROTOCOL_NODESTATUS_HEALTH_OK;
-static uint8_t node_mode   = UAVCAN_PROTOCOL_NODESTATUS_MODE_INITIALIZATION;
+static uavcan_protocol_NodeStatus node_status;
 
 
 static uint64_t getMonotonicTimestampUSec(void)
@@ -101,30 +86,11 @@ static float getRandomFloat(void)
  */
 static void readUniqueID(uint8_t* out_uid)
 {
-    uint8_t len = UNIQUE_ID_LENGTH_BYTES;
+    uint8_t len = UAVCAN_PROTOCOL_DYNAMIC_NODE_ID_ALLOCATION_UNIQUE_ID_MAX_LENGTH;
     memset(out_uid, 0, len);
     hal.util->get_system_id_unformatted(out_uid, len);
 }
 
-
-static uint32_t makeNodeStatusMessage(uint8_t buffer[UAVCAN_PROTOCOL_NODESTATUS_MAX_SIZE])
-{
-    uavcan_protocol_NodeStatus status {};
-
-    static uint32_t started_at_sec = 0;
-    if (started_at_sec == 0)
-    {
-        started_at_sec = (uint32_t)(getMonotonicTimestampUSec() / 1000000U);
-    }
-
-    status.uptime_sec = (uint32_t)((getMonotonicTimestampUSec() / 1000000U) - started_at_sec);
-    status.health = node_health;
-    status.mode = node_mode;
-    status.sub_mode = 0;
-    status.vendor_specific_status_code = 0;
-
-    return uavcan_protocol_NodeStatus_encode(&status, buffer);
-}
 
 /*
   handle a GET_NODE_INFO request
@@ -132,43 +98,33 @@ static uint32_t makeNodeStatusMessage(uint8_t buffer[UAVCAN_PROTOCOL_NODESTATUS_
 static void handle_get_node_info(CanardInstance* ins,
                                  CanardRxTransfer* transfer)
 {
-    uint8_t buffer[UAVCAN_GET_NODE_INFO_RESPONSE_MAX_SIZE] {};
+    uint8_t buffer[UAVCAN_PROTOCOL_GETNODEINFO_RESPONSE_MAX_SIZE] {};
+    uavcan_protocol_GetNodeInfoResponse pkt {};
 
-    // NodeStatus
-    makeNodeStatusMessage(buffer);
+    node_status.uptime_sec = AP_HAL::millis() / 1000U;
 
-    // SoftwareVersion
-    buffer[7] = APP_VERSION_MAJOR;
-    buffer[8] = APP_VERSION_MINOR;
-    buffer[9] = 1;                          // Optional field flags, VCS commit is set
-    uint32_t u32 = 1234; // XXXX git hash
-    canardEncodeScalar(buffer, 80, 32, &u32);
-    // Image CRC skipped
+    pkt.status = node_status;
+    pkt.software_version.major = APP_VERSION_MAJOR;
+    pkt.software_version.minor = APP_VERSION_MINOR;
 
-    // HardwareVersion
-    // Major skipped
-    // Minor skipped
-    readUniqueID(&buffer[24]);
-    // Certificate of authenticity skipped
+    readUniqueID(pkt.hardware_version.unique_id);
 
-    // Name
-    const size_t name_len = strlen(APP_NODE_NAME);
-    memcpy(&buffer[41], APP_NODE_NAME, name_len);
+    char name[strlen(APP_NODE_NAME)+1];
+    strcpy(name, APP_NODE_NAME);
+    pkt.name.len = strlen(APP_NODE_NAME);
+    pkt.name.data = (uint8_t *)name;
 
-    const size_t total_size = 41 + name_len;
+    uint16_t total_size = uavcan_protocol_GetNodeInfoResponse_encode(&pkt, buffer);
 
-    /*
-     * Transmitting; in this case we don't have to release the payload because it's empty anyway.
-     */
     const int16_t resp_res = canardRequestOrRespond(ins,
                                                     transfer->source_node_id,
-                                                    UAVCAN_GET_NODE_INFO_DATA_TYPE_SIGNATURE,
-                                                    UAVCAN_GET_NODE_INFO_DATA_TYPE_ID,
+                                                    UAVCAN_PROTOCOL_GETNODEINFO_SIGNATURE,
+                                                    UAVCAN_PROTOCOL_GETNODEINFO_ID,
                                                     &transfer->transfer_id,
                                                     transfer->priority,
                                                     CanardResponse,
                                                     &buffer[0],
-                                                    (uint16_t)total_size);
+                                                    total_size);
     if (resp_res <= 0) {
         puts("Could not respond to GetNodeInfo");
     }
@@ -233,17 +189,17 @@ static void onTransferReceived(CanardInstance* ins,
 
         // Copying the unique ID from the message
         static const uint8_t UniqueIDBitOffset = 8;
-        uint8_t received_unique_id[UNIQUE_ID_LENGTH_BYTES];
+        uint8_t received_unique_id[UAVCAN_PROTOCOL_DYNAMIC_NODE_ID_ALLOCATION_UNIQUE_ID_MAX_LENGTH];
         uint8_t received_unique_id_len = 0;
         for (; received_unique_id_len < (transfer->payload_len - (UniqueIDBitOffset / 8U)); received_unique_id_len++)
         {
-            assert(received_unique_id_len < UNIQUE_ID_LENGTH_BYTES);
+            assert(received_unique_id_len < UAVCAN_PROTOCOL_DYNAMIC_NODE_ID_ALLOCATION_UNIQUE_ID_MAX_LENGTH);
             const uint8_t bit_offset = (uint8_t)(UniqueIDBitOffset + received_unique_id_len * 8U);
             (void) canardDecodeScalar(transfer, bit_offset, 8, false, &received_unique_id[received_unique_id_len]);
         }
 
         // Obtaining the local unique ID
-        uint8_t my_unique_id[UNIQUE_ID_LENGTH_BYTES];
+        uint8_t my_unique_id[UAVCAN_PROTOCOL_DYNAMIC_NODE_ID_ALLOCATION_UNIQUE_ID_MAX_LENGTH];
         readUniqueID(my_unique_id);
 
         // Matching the received UID against the local one
@@ -275,7 +231,7 @@ static void onTransferReceived(CanardInstance* ins,
     }
 
     switch (transfer->data_type_id) {
-    case UAVCAN_GET_NODE_INFO_DATA_TYPE_ID:
+    case UAVCAN_PROTOCOL_GETNODEINFO_ID:
         handle_get_node_info(ins, transfer);
         break;
 
@@ -321,8 +277,8 @@ static bool shouldAcceptTransfer(const CanardInstance* ins,
     }
 
     switch (data_type_id) {
-    case UAVCAN_GET_NODE_INFO_DATA_TYPE_ID:
-        *out_data_type_signature = UAVCAN_GET_NODE_INFO_DATA_TYPE_SIGNATURE;
+    case UAVCAN_PROTOCOL_GETNODEINFO_ID:
+        *out_data_type_signature = UAVCAN_PROTOCOL_GETNODEINFO_SIGNATURE;
         return true;
     case UAVCAN_PROTOCOL_FILE_BEGINFIRMWAREUPDATE_ID:
         *out_data_type_signature = UAVCAN_PROTOCOL_FILE_BEGINFIRMWAREUPDATE_SIGNATURE;
@@ -416,8 +372,9 @@ static void process1HzTasks(uint64_t timestamp_usec)
      */
     {
         uint8_t buffer[UAVCAN_PROTOCOL_NODESTATUS_MAX_SIZE];
-        uint32_t len = makeNodeStatusMessage(buffer);
+        node_status.uptime_sec = AP_HAL::millis() / 1000U;
 
+        uint32_t len = uavcan_protocol_NodeStatus_encode(&node_status, buffer);
 
         static uint8_t transfer_id;  // Note that the transfer ID variable MUST BE STATIC (or heap-allocated)!
 
@@ -436,7 +393,7 @@ static void process1HzTasks(uint64_t timestamp_usec)
         }
     }
 
-    node_mode = UAVCAN_PROTOCOL_NODESTATUS_MODE_OPERATIONAL;
+    node_status.mode = UAVCAN_PROTOCOL_NODESTATUS_MODE_OPERATIONAL;
 }
 
 /*
@@ -477,21 +434,21 @@ static void can_wait_node_id(void)
             allocation_request[0] |= 1;     // First part of unique ID
         }
 
-        uint8_t my_unique_id[UNIQUE_ID_LENGTH_BYTES];
+        uint8_t my_unique_id[UAVCAN_PROTOCOL_DYNAMIC_NODE_ID_ALLOCATION_UNIQUE_ID_MAX_LENGTH];
         readUniqueID(my_unique_id);
 
         static const uint8_t MaxLenOfUniqueIDInRequest = 6;
-        uint8_t uid_size = (uint8_t)(UNIQUE_ID_LENGTH_BYTES - node_id_allocation_unique_id_offset);
+        uint8_t uid_size = (uint8_t)(UAVCAN_PROTOCOL_DYNAMIC_NODE_ID_ALLOCATION_UNIQUE_ID_MAX_LENGTH - node_id_allocation_unique_id_offset);
         if (uid_size > MaxLenOfUniqueIDInRequest)
         {
             uid_size = MaxLenOfUniqueIDInRequest;
         }
 
         // Paranoia time
-        assert(node_id_allocation_unique_id_offset < UNIQUE_ID_LENGTH_BYTES);
+        assert(node_id_allocation_unique_id_offset < UAVCAN_PROTOCOL_DYNAMIC_NODE_ID_ALLOCATION_UNIQUE_ID_MAX_LENGTH);
         assert(uid_size <= MaxLenOfUniqueIDInRequest);
         assert(uid_size > 0);
-        assert((uid_size + node_id_allocation_unique_id_offset) <= UNIQUE_ID_LENGTH_BYTES);
+        assert((uid_size + node_id_allocation_unique_id_offset) <= UAVCAN_PROTOCOL_DYNAMIC_NODE_ID_ALLOCATION_UNIQUE_ID_MAX_LENGTH);
 
         memmove(&allocation_request[1], &my_unique_id[node_id_allocation_unique_id_offset], uid_size);
 
@@ -517,6 +474,10 @@ static void can_wait_node_id(void)
 
 void AP_Periph_FW::can_start()
 {
+    node_status.health = UAVCAN_PROTOCOL_NODESTATUS_HEALTH_OK;
+    node_status.mode = UAVCAN_PROTOCOL_NODESTATUS_MODE_INITIALIZATION;
+    node_status.uptime_sec = AP_HAL::millis() / 1000U;
+
     canStart(&CAND1, &cancfg);
 
     canardInit(&canard, (uint8_t *)canard_memory_pool, sizeof(canard_memory_pool),
