f3a6cd8462d948125fd48ac7bb7421ce489fa63e
Andrew Tridgell <andrew@tridgell.net>
2019-03-13 12:00:42 +1100

    AP_NavEKF3: allow non-zero alt reset

--- libraries/AP_NavEKF3/AP_NavEKF3_PosVelFusion.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_PosVelFusion.cpp
@@ -199,7 +199,7 @@ void NavEKF3_core::ResetHeight(void)
 
 // Zero the EKF height datum
 // Return true if the height datum reset has been performed
-bool NavEKF3_core::resetHeightDatum(void)
+bool NavEKF3_core::resetHeightDatum(float alt_m)
 {
     if (activeHgtSource == HGT_SOURCE_RNG || !onGround) {
         // only allow resets when on the ground.
@@ -210,9 +210,9 @@ bool NavEKF3_core::resetHeightDatum(void)
     // record the old height estimate
     float oldHgt = -stateStruct.position.z;
     // reset the barometer so that it reads zero at the current height
-    AP::baro().update_calibration();
+    AP::baro().update_calibration(alt_m);
     // reset the height state
-    stateStruct.position.z = 0.0f;
+    stateStruct.position.z = -alt_m;
     // adjust the height of the EKF origin so that the origin plus baro height before and after the reset is the same
     if (validOrigin) {
         if (!gpsGoodToAlign) {
@@ -225,15 +225,14 @@ bool NavEKF3_core::resetHeightDatum(void)
             // altitude. This ensures the reported AMSL alt from
             // getLLH() is equal to GPS altitude, while also ensuring
             // that the relative alt is zero
-            EKF_origin.alt = AP::gps().location().alt;
+            EKF_origin.alt = AP::gps().location().alt - alt_m;
         }
         ekfGpsRefHgt = (double)0.01 * (double)EKF_origin.alt;
     }
 
     // set the terrain state to zero (on ground). The adjustment for
     // frame height will get added in the later constraints
-    terrainState = 0;
-
+    terrainState = alt_m;
     return true;
 }
 
