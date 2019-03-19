753919840dec84bf7b4cb1ea13cfb484edfe5c05
Andrew Tridgell <andrew@tridgell.net>
2019-03-20 07:44:58 +1100

    AP_HAL: added LIS3MDL_I2C driver selection

--- libraries/AP_HAL/AP_HAL_Boards.h
+++ libraries/AP_HAL/AP_HAL_Boards.h
@@ -131,6 +131,7 @@
 #define HAL_COMPASS_BMM150_I2C         21
 #define HAL_COMPASS_QMC5883L           22
 #define HAL_COMPASS_IST8310            23
+#define HAL_COMPASS_LIS3MDL_I2C        24
 
 /* Heat Types */
 #define HAL_LINUX_HEAT_PWM 1
