472edfe6968f47b4198fac7fd35fb549022a69ec
Andrew Tridgell <andrew@tridgell.net>
2019-03-21 18:45:32 +1100

    AP_Periph: added parameter code

---
+++ Tools/AP_Periph/Parameters.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <AP_Common/AP_Common.h>
+
+// Global parameter class.
+//
+class Parameters {
+public:
+    static const uint16_t k_format_version = 1;
+
+    enum {
+        // Layout version number, always key zero.
+        //
+        k_param_format_version = 0,
+        k_param_gps,
+        k_param_serial_manager,
+        k_param_compass,
+    };
+
+    AP_Int16 format_version;
+
+    Parameters() {}
+};
+
+extern const AP_Param::Info var_info[];
