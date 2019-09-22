4ea7e0f0e3579bd374059b617c6a3b84134bc128
Peter Barker <pbarker@barker.dropbear.id.au>
2019-09-22 11:07:23 +1000

    Tools: autotest: add test for Plane follow mode

--- Tools/autotest/common.py
+++ Tools/autotest/common.py
@@ -8862,8 +8862,6 @@ switch value'''
                     self.progress("  Fulfilled")
                     del wants[want]
 
-
-
     def test_crsf(self):
         self.context_push()
         ex = None
@@ -8893,6 +8891,19 @@ switch value'''
         self.reboot_sitl()
         if ex is not None:
             raise ex
+
+    def global_position_int_for_location(self, loc, time_boot, heading=0):
+        return self.mav.mav.global_position_int_encode(
+            int(time_boot * 1000), # time_boot_ms
+            int(loc.lat * 1e7),
+            int(loc.lng * 1e7),
+            int(loc.alt * 1000), # alt in mm
+            20, # relative alt - urp.
+            vx = 0,
+            vy = 0,
+            vz = 0,
+            hdg = heading
+        )
 
     def tests(self):
         return [
