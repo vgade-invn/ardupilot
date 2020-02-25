cfa69dca5608b8fccc590143f75c64519d568604
Dan Laks <dan@spektreworks.com>
2020-02-25 12:13:44 -0700

    ArduPlane V4.0.1SW2 Release

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V4.0.7"
+#define THISFIRMWARE "ArduPlane V4.0.7SW1"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,0,7,FIRMWARE_VERSION_TYPE_OFFICIAL
