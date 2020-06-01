3f2a5ca1c67fab2de392b5710f81bbce03d18374
Iampete1 <iampete@hotmail.co.uk>
2020-06-01 01:21:10 +0100

    JSON: update matlab example

--- libraries/SITL/examples/JSON/MATLAB/SITL_connector.m
+++ libraries/SITL/examples/JSON/MATLAB/SITL_connector.m
@@ -31,7 +31,7 @@ pnet(u,'setreadtimeout',0);
 
 frame_time = tic;
 frame_count = 0;
-physics_time_us = 0;
+physics_time_s = 0;
 last_SITL_frame = -1;
 print_frame_count = 1000; % print the fps every x frames
 connected = false;
@@ -82,7 +82,7 @@ while true
         fprintf('Missed %i input frames\n',SITL_frame - last_SITL_frame - 1)
     end
     last_SITL_frame = SITL_frame;
-    physics_time_us = physics_time_us + delta_t * 10^6;
+    physics_time_s = physics_time_s + delta_t;
 
     if ~connected
         % use port -1 to indicate connection to address of last recv pkt
@@ -95,7 +95,7 @@ while true
     state = physics_function(pwm_in,state);
 
     % build structure representing the JSON string to be sent
-    JSON.timestamp = physics_time_us;
+    JSON.timestamp = physics_time_s;
     JSON.imu.gyro = state.gyro;
     JSON.imu.accel_body = state.accel;
     JSON.position = state.position;
