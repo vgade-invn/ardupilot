1a56c043878da948b2fa3c17448910a5fc823c9c
Andrew Tridgell <andrew@tridgell.net>
2020-07-03 17:02:06 +1000

    AP_Scripting: added standoff distance for ship landing

--- libraries/AP_Scripting/examples/ship_landing.lua
+++ libraries/AP_Scripting/examples/ship_landing.lua
@@ -9,6 +9,9 @@ local HOLD_HEIGHT = 50
 -- flight mode numbers for plane
 local MODE_GUIDED = 15
 
+-- standoff distance for loiter
+local STANDOFF_DIST = 150
+
 -- location, velocity and heading of target beacon
 local target = Location()
 local target_velocity = Vector3f()
@@ -17,17 +20,28 @@ local target_hdg_cd = 0
 -- local time of last msg from beacon
 local last_update_ms = 0
 
+local UINT16_MAX = 65535
+
+function radians(x)
+   return x * 0.0174532925199
+end
+
 -- update target variables
 function update_target()
    if not vehicle:set_follow_sysid(BEACON_SYS_ID) then
       return false
    end
-   local ts1 = vehicle:get_follow_last_update_ms()
-   target = vehicle:get_follow_location()
-   target_velocity = vehicle:get_follow_velocity()
-   target_hdg_cd = vehicle:get_follow_heading()
-   ts2 = vehicle:get_follow_last_update_ms()
-   if ts1:tofloat() == 0 or ts1:tofloat() ~= ts2:tofloat() then
+   -- we loop until we get the same timestamp twice, avoiding a race condition in fetch data
+   -- from multiple GLOBAL_POSITION_INT packets
+   local ts1, ts2
+   repeat
+      ts1 = vehicle:get_follow_last_update_ms()
+      target = vehicle:get_follow_location()
+      target_velocity = vehicle:get_follow_velocity()
+      target_hdg_cd = vehicle:get_follow_heading()
+      ts2 = vehicle:get_follow_last_update_ms()
+   until ts1 == ts2
+   if ts1 == uint32_t(0) then
       return false
    end
    last_update_ms = ts1
@@ -52,7 +66,24 @@ function update()
       return update, 200
    end
 
-   target:alt(home:alt() + HOLD_HEIGHT*100)
+   -- advnce target by time delta to account for comms lag
+   local dt = (millis() - last_update_ms):tofloat() * 0.001
+   local dpos = target_velocity:scale(dt)
+   target:offset(dpos:x(), dpos:y())
+
+   -- hold at HOLD_HEIGHT above target
+   target:alt(target:alt() + HOLD_HEIGHT*100)
+
+   -- hold to rear and right of target by STANDOFF_DIST
+   if target_hdg_cd ~= UINT16_MAX then
+      hdg_rad = radians(target_hdg_cd*0.01)
+      offset = Vector2f()
+      offset:x(-STANDOFF_DIST)
+      offset:y(STANDOFF_DIST)
+      offset:rotate(hdg_rad)
+      target:offset(offset:x(), offset:y())
+   end
+
    vehicle:set_target_location(target)
 
    local dist = current:get_distance(target)
