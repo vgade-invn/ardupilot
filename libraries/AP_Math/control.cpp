71910855537ce8e54fa26d761b700a934347b6c3
Leonard Hall <leonardthall@gmail.com>
2020-07-13 21:07:18 +0930

    AP_Math: Add input shaping to control tools.

--- libraries/AP_Math/control.cpp
+++ libraries/AP_Math/control.cpp
@@ -22,10 +22,195 @@
 #include "AP_Math.h"
 #include "vector2.h"
 #include "vector3.h"
+#include <AP_Logger/AP_Logger.h>
 
+// shape_pos_vel_accel_xy takes the current pos, vel, accel, vel and accel limits and a time constant.
+// The current acceleration is adjusted with jerk limiting to achieve the desired pos and vel.
+void update_pos_vel_accel(float& pos, float& vel, float& accel, float dt)
+{
+    // move pos, vel to current time frame
+    pos = pos + vel * dt + accel * 0.5f * sq(dt);
+    vel = vel + accel * dt;
+}
+
+// shape_pos_vel_accel_xy takes the current pos, vel, accel, vel and accel limits and a time constant.
+// The current acceleration is adjusted with jerk limiting to achieve the desired pos and vel.
+void update_pos_vel_accel_xy(Vector2f& pos, Vector2f& vel, Vector2f& accel, float dt)
+{
+    // move pos, vel to current time frame
+    pos = pos + vel * dt + accel * 0.5f * sq(dt);
+    vel = vel + accel * dt;
+}
+
+void update_pos_vel_accel_xy(Vector3f& pos, Vector3f& vel, Vector3f& accel, float dt)
+{
+    Vector2f pos_2f = Vector2f(pos.x, pos.y);
+    Vector2f vel_2f = Vector2f(vel.x, vel.y);
+    Vector2f accel_2f = Vector2f(accel.x, accel.y);
+    update_pos_vel_accel_xy(pos, vel, accel, dt);
+    pos.x = pos_2f.x;
+    pos.y = pos_2f.y;
+    vel.x = vel_2f.x;
+    vel.y = vel_2f.y;
+    accel.x = accel_2f.x;
+    accel.y = accel_2f.y;
+}
+
+// shape_pos_vel_accel_xy takes the current pos, vel, accel, vel and accel limits and a time constant.
+// The current acceleration is adjusted with jerk limiting to achieve the desired pos and vel.
+void shape_vel_accel(float& vel_target, const float& vel, float& accel, float vel_max, float accel_max, float tc, float dt)
+{
+    // Calculate time constants and limits to ensure stable operation
+    float KPa = 1.0 / tc;
+    float jerk_max = accel_max * KPa;
+
+    // limit velocity to vel_max
+    vel_target = constrain_float(vel_target, -vel_max, vel_max);
+
+    // velocity error to be corrected
+    float vel_error = vel_target - vel;
+
+    // acceleration to correct velocity
+    float accel_target = vel_error * KPa;
+
+    // jerk limit acceleration change
+    float accel_delta = accel_target - accel;
+    accel_delta = constrain_float(accel_delta, -jerk_max * dt, jerk_max * dt);
+    accel = accel + accel_delta;
+
+    // limit acceleration to accel_max
+    accel = constrain_float(accel, -accel_max, accel_max);
+
+    // Calculate maximum pos_des and vel_des based on limited system
+    vel_error = accel / KPa;
+    vel_target = vel_error + vel;
+}
+
+// shape_pos_vel_accel_xy takes the current pos, vel, accel, vel and accel limits and a time constant.
+// The current acceleration is adjusted with jerk limiting to achieve the desired pos and vel.
+void shape_vel_accel_xy(Vector2f& vel_target, const Vector2f& vel, Vector2f& accel, float vel_max, float accel_max, float tc, float dt)
+{
+    // Calculate time constants and limits to ensure stable operation
+    float KPa = 1.0 / tc;
+    float jerk_max = accel_max * KPa;
+
+    // limit velocity to vel_max
+    limit_vector_length(vel_target.x, vel_target.y, vel_max);
+
+    // velocity error to be corrected
+    Vector2f vel_error = vel_target - vel;
+
+    // acceleration to correct velocity
+    Vector2f accel_target = vel_error * KPa;
+
+    // jerk limit acceleration change
+    Vector2f accel_delta = accel_target - accel;
+    limit_vector_length(accel_delta.x, accel_delta.y, jerk_max * dt);
+    accel = accel + accel_delta;
+
+    // limit acceleration to accel_max
+    limit_vector_length(accel.x, accel.y, accel_max);
+
+    // Calculate maximum pos_des and vel_des based on limited system
+    vel_error = accel / KPa;
+    vel_target = vel_error + vel;
+}
+
+void shape_vel_accel_xy(Vector3f& vel_des, const Vector3f& vel, Vector3f& accel, float vel_max, float accel_max, float tc, float dt)
+{
+    Vector2f vel_des_2f = Vector2f(vel_des.x, vel_des.y);
+    Vector2f vel_2f = Vector2f(vel.x, vel.y);
+    Vector2f accel_2f = Vector2f(accel.x, accel.y);
+
+    shape_vel_accel_xy(vel_des_2f, vel_2f, accel_2f, vel_max, accel_max, tc, dt);
+    vel_des.x = vel_des_2f.x;
+    vel_des.y = vel_des_2f.y;
+    accel.x = accel_2f.x;
+    accel.y = accel_2f.y;
+}
+
+// shape_pos_vel_accel_xy takes the current pos, vel, accel, vel and accel limits and a time constant.
+// The current acceleration is adjusted with jerk limiting to achieve the desired pos and vel.
+void shape_pos_vel_accel(float& pos_des, const float& vel_des, const float& pos, const float& vel, float& accel, float vel_max, float accel_max, float tc, float dt)
+{
+    // Calculate time constants and limits to ensure stable operation
+    float KPv = 1.0 / (CONTROL_TIME_CONSTANT_RATIO*tc);
+    float accel_tc_max = accel_max*(1-1.0f/CONTROL_TIME_CONSTANT_RATIO);
+
+    // position error to be corrected
+    float pos_error = pos_des - pos;
+
+    // velocity to correct position
+    float vel_target = sqrt_controller(pos_error, KPv, accel_tc_max, dt);
+
+    // velocity correction with desired velocity
+    vel_target = vel_target + vel_des;
+
+    shape_vel_accel(vel_target, vel, accel, vel_max, accel_max, tc, dt);
+
+    vel_target = vel_target - vel_des;
+    pos_error = stopping_point(vel_target, KPv, accel_tc_max);
+    pos_des = pos_error + pos;
+}
+
+// shape_pos_vel_accel_xy takes the current pos, vel, accel, vel and accel limits and a time constant.
+// The current acceleration is adjusted with jerk limiting to achieve the desired pos and vel.
+void shape_pos_vel_accel_xy(Vector2f& pos_des, const Vector2f& vel_des, const Vector2f& pos, const Vector2f& vel, Vector2f& accel, float vel_max, float accel_max, float tc, float dt)
+{
+    // Calculate time constants and limits to ensure stable operation
+    float KPv = 1.0 / (CONTROL_TIME_CONSTANT_RATIO*tc);
+    float accel_tc_max = accel_max*(1-1.0f/CONTROL_TIME_CONSTANT_RATIO);
+
+    // position error to be corrected
+    Vector2f pos_error = pos_des - pos;
+
+    // velocity to correct position
+    Vector2f vel_target = sqrt_controller_xy(pos_error, KPv, accel_tc_max);
+
+    // velocity correction with desired velocity
+    vel_target = vel_target + vel_des;
+
+    shape_vel_accel_xy(vel_target, vel, accel, vel_max, accel_max, tc, dt);
+
+    vel_target = vel_target - vel_des;
+    pos_error = stopping_point_xy(vel_target, KPv, accel_tc_max);
+    pos_des = pos_error + pos;
+
+    AP::logger().Write("SHP",
+                       "TimeUS,DPX,DPY,PVX,PVY,PX,PY,VX,VY,AX,AY",
+                       "soooooooooo",
+                       "F0000000000",
+                       "Qffffffffff",
+                       AP_HAL::micros64(),
+                       double(pos_des.x * 0.01f),
+                       double(pos_des.y * 0.01f),
+                       double(vel_des.x * 0.01f),
+                       double(vel_des.y * 0.01f),
+                       double(pos.x * 0.01f),
+                       double(pos.y * 0.01f),
+                       double(vel.x * 0.01f),
+                       double(vel.y * 0.01f),
+                       double(accel.x * 0.01f),
+                       double(accel.y * 0.01f));
+}
+
+void shape_pos_vel_accel_xy(Vector3f& pos_des, const Vector3f& vel_des, const Vector3f& pos, const Vector3f& vel, Vector3f& accel, float vel_max, float accel_max, float tc, float dt)
+{
+    Vector2f pos_des_2f = Vector2f(pos_des.x, pos_des.y);
+    Vector2f vel_des_2f = Vector2f(vel_des.x, vel_des.y);
+    Vector2f pos_2f = Vector2f(pos.x, pos.y);
+    Vector2f vel_2f = Vector2f(vel.x, vel.y);
+    Vector2f accel_2f = Vector2f(accel.x, accel.y);
+
+    shape_pos_vel_accel_xy(pos_des_2f, vel_des_2f, pos_2f, vel_2f, accel_2f, vel_max, accel_max, tc, dt);
+    pos_des.x = pos_des_2f.x;
+    pos_des.y = pos_des_2f.y;
+    accel.x = accel_2f.x;
+    accel.y = accel_2f.y;
+}
 
 // Proportional controller with piecewise sqrt sections to constrain second derivative
-float sqrt_controller(float error, float p, float second_ord_lim, float dt)
+float sqrt_controller(const float& error, float p, float second_ord_lim, float dt)
 {
     float correction_rate;
     if (is_negative(second_ord_lim) || is_zero(second_ord_lim)) {
--- libraries/AP_Math/control.h
+++ libraries/AP_Math/control.h
@@ -4,8 +4,29 @@
  * LOCATION
  */
 
+// control default definitions
+#define CONTROL_TIME_CONSTANT_RATIO 4.0f   // minimum horizontal acceleration in cm/s/s - used for sanity checking acceleration in leash length calculation
+
+// shape_pos_vel_accel_xy takes the current pos, vel, accel, vel and accel limits and a time constant.
+// The current acceleration is adjusted with jerk limiting to achieve the desired pos and vel.
+void update_pos_vel_accel(float& pos, float& vel, float& accel, float dt);
+void update_pos_vel_accel_xy(Vector2f& pos, Vector2f& vel, Vector2f& accel, float dt);
+void update_pos_vel_accel_xy(Vector3f& pos, Vector3f& vel, Vector3f& accel, float dt);
+
+// shape_pos_vel_accel_xy takes the current pos, vel, accel, vel and accel limits and a time constant.
+// The current acceleration is adjusted with jerk limiting to achieve the desired pos and vel.
+void shape_vel_accel(float& vel_target, const float& vel, float& accel, float vel_max, float accel_max, float tc, float dt);
+void shape_vel_accel_xy(Vector2f& vel_target, const Vector2f& vel, Vector2f& accel, float vel_max, float accel_max, float tc, float dt);
+void shape_vel_accel_xy(Vector3f& vel_des, const Vector3f& vel, Vector3f& accel, float vel_max, float accel_max, float tc, float dt);
+
+// shape_pos_vel_accel_xy takes the current pos, vel, accel, vel and accel limits and a time constant.
+// The current acceleration is adjusted with jerk limiting to achieve the desired pos and vel.
+void shape_pos_vel_accel(float& pos_des, const float& vel_des, const float& pos, const float& vel, float& accel, float vel_max, float accel_max, float tc, float dt);
+void shape_pos_vel_accel_xy(Vector2f& pos_des, const Vector2f& vel_des, const Vector2f& pos, const Vector2f& vel, Vector2f& accel, float vel_max, float accel_max, float tc, float dt);
+void shape_pos_vel_accel_xy(Vector3f& pos_des, const Vector3f& vel_des, const Vector3f& pos, const Vector3f& vel, Vector3f& accel, float vel_max, float accel_max, float tc, float dt);
+
 // Proportional controller with piecewise sqrt sections to constrain second derivative
-float sqrt_controller(float error, float p, float second_ord_lim, float dt);
+float sqrt_controller(const float& error, float p, float second_ord_lim, float dt);
 
 /// Proportional controller with piecewise sqrt sections to constrain second derivative
 Vector2f sqrt_controller_xy(const Vector2f& error, float p, float second_ord_lim);
