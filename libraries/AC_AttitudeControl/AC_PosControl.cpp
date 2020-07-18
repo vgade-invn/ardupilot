f8ebf543e7a43cd8ab92c2db5f14ea94c7268052
Leonard Hall <leonardthall@gmail.com>
2020-07-18 21:40:10 +0930

    AC_PosControl: init for pos_vel_xy

--- libraries/AC_AttitudeControl/AC_PosControl.cpp
+++ libraries/AC_AttitudeControl/AC_PosControl.cpp
@@ -855,7 +855,42 @@ void AC_PosControl::standby_xyz_reset()
 }
 
 /// update_xy_controller - run the horizontal position controller - should be called at 100hz or higher
-void AC_PosControl::input_vel_accel_xy(Vector3f vel, float vel_max, float accel_max, float tc)
+void AC_PosControl::init_pos_vel_xy()
+{
+    // set roll, pitch lean angle targets to current attitude
+    // todo: this should probably be based on the desired attitude not the current attitude
+    _roll_target = _ahrs.roll_sensor;
+    _pitch_target = _ahrs.pitch_sensor;
+
+
+    Vector3f curr_pos = _inav.get_position();
+    _pos_target.x = curr_pos.x;
+    _pos_target.y = curr_pos.y;
+
+    const Vector3f& curr_vel = _inav.get_velocity();
+    _vel_desired.x = curr_vel.x;
+    _vel_desired.y = curr_vel.y;
+
+    _accel_desired.x = 0.0f;
+    _accel_desired.y = 0.0f;
+
+    // initialise I terms from lean angles
+    _vel_target.x = 0.0f;
+    _vel_target.y = 0.0f;
+    _pid_vel_xy.reset_filter();
+    lean_angles_to_accel(_accel_target.x, _accel_target.y);
+    _pid_vel_xy.set_integrator(_accel_target);
+
+    // flag reset required in rate to accel step
+    _flags.reset_desired_vel_to_pos = true;
+    _flags.reset_accel_to_lean_xy = true;
+
+    // initialise ekf xy reset handler
+    init_ekf_xy_reset();
+}
+
+/// update_xy_controller - run the horizontal position controller - should be called at 100hz or higher
+void AC_PosControl::input_vel_xy(Vector3f vel, float vel_max, float accel_max, float tc)
 {
     // compute dt
     const uint64_t now_us = AP_HAL::micros64();
@@ -872,7 +907,11 @@ void AC_PosControl::input_vel_accel_xy(Vector3f vel, float vel_max, float accel_
     // check if xy leash needs to be recalculated
     calc_leash_length_xy();
 
-    update_pos_vel_accel_xy(_pos_target, _vel_desired, _accel_desired, dt);
+    if (_flags.reset_desired_vel_to_pos ) {
+        _flags.reset_desired_vel_to_pos = false;
+    } else {
+        update_pos_vel_accel_xy(_pos_target, _vel_desired, _accel_desired, dt);
+    }
     shape_vel_accel_xy(vel, _vel_desired, _accel_desired, vel_max, accel_max, tc, dt);
 
     // run horizontal position controller
@@ -883,7 +922,7 @@ void AC_PosControl::input_vel_accel_xy(Vector3f vel, float vel_max, float accel_
 }
 
 /// update_xy_controller - run the horizontal position controller - should be called at 100hz or higher
-void AC_PosControl::input_pos_vel_accel_xy(Vector3f pos, Vector3f vel, float vel_max, float accel_max, float tc)
+void AC_PosControl::input_pos_vel_xy(Vector3f pos, Vector3f vel, float vel_max, float accel_max, float tc)
 {
     // compute dt
     const uint64_t now_us = AP_HAL::micros64();
@@ -900,7 +939,11 @@ void AC_PosControl::input_pos_vel_accel_xy(Vector3f pos, Vector3f vel, float vel
     // check if xy leash needs to be recalculated
     calc_leash_length_xy();
 
-    update_pos_vel_accel_xy(_pos_target, _vel_desired, _accel_desired, dt);
+    if (_flags.reset_desired_vel_to_pos ) {
+        _flags.reset_desired_vel_to_pos = false;
+    } else {
+        update_pos_vel_accel_xy(_pos_target, _vel_desired, _accel_desired, dt);
+    }
     shape_pos_vel_accel_xy(pos, vel, _pos_target, _vel_desired, _accel_desired, vel_max, accel_max, tc, dt);
 
     // run horizontal position controller
@@ -1118,10 +1161,10 @@ void AC_PosControl::run_xy_controller(float dt)
         // Constrain _pos_error and target position
         // Constrain the maximum length of _vel_target to the maximum position correction velocity
         // TODO: replace the leash length with a user definable maximum position correction
-        if (limit_vector_length(_pos_error.x, _pos_error.y, _leash)) {
-            _pos_target.x = curr_pos.x + _pos_error.x;
-            _pos_target.y = curr_pos.y + _pos_error.y;
-        }
+//        if (limit_vector_length(_pos_error.x, _pos_error.y, _leash)) {
+//            _pos_target.x = curr_pos.x + _pos_error.x;
+//            _pos_target.y = curr_pos.y + _pos_error.y;
+//        }
 
         _vel_target = sqrt_controller_xy(_pos_error, kP, _accel_cms);
     }
