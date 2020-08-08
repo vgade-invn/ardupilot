8369c83ea9b1045a925f90d6240a8dbb95944487
Andrew Tridgell <andrew@tridgell.net>
2020-08-08 15:52:02 +1000

    AP_Vehicle: added FLIGHT_AIRBRAKE stage
    used by TECS for VTOL landing approach code

--- libraries/AP_Vehicle/AP_Vehicle.h
+++ libraries/AP_Vehicle/AP_Vehicle.h
@@ -96,7 +96,8 @@ public:
             FLIGHT_VTOL          = 2,
             FLIGHT_NORMAL        = 3,
             FLIGHT_LAND          = 4,
-            FLIGHT_ABORT_LAND    = 7
+            FLIGHT_ABORT_LAND    = 7,
+            FLIGHT_AIRBRAKE      = 8,
         };
     };
 
