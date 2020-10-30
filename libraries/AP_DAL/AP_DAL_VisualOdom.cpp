e08444c9633f5b9028bddb5197d7534916028451
Peter Barker <pbarker@barker.dropbear.id.au>
2020-10-30 15:41:55 +1100

    handle VisualOdom

---
+++ libraries/AP_DAL/AP_DAL_VisualOdom.cpp
@@ -0,0 +1,26 @@
+#include "AP_DAL_VisualOdom.h"
+
+#include <AP_VisualOdom/AP_VisualOdom.h>
+
+#include <AP_Logger/AP_Logger.h>
+
+AP_DAL_VisualOdom::AP_DAL_VisualOdom()
+{
+    RVOH.head1 = HEAD_BYTE1;
+    RVOH.head2 = HEAD_BYTE2;
+    RVOH.msgid = LOG_RVOH_MSG;
+}
+
+void AP_DAL_VisualOdom::start_frame(const uint64_t time_us)
+{
+    const auto *vo = AP::visualodom();
+
+    auto &logger = AP::logger();
+
+    RVOH.time_us = time_us;
+    RVOH.ptr_is_nullptr = (vo == nullptr);
+    if (vo != nullptr) {
+        RVOH.healthy = vo->healthy();
+    }
+    logger.WriteReplayBlock((void*)&RVOH, sizeof(RVOH));
+}
