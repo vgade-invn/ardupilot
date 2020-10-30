ba8b7e55fb046a7b94934399c644bf9bbb66319f
Andrew Tridgell <andrew@tridgell.net>
2020-10-31 08:45:52 +1100

    AP_DAL: removed tmp vars

--- libraries/AP_DAL/AP_DAL_GPS.cpp
+++ libraries/AP_DAL/AP_DAL_GPS.cpp
@@ -41,26 +41,18 @@ void AP_DAL_GPS::start_frame(const uint64_t time_us)
         RGPI.alt = loc.alt;
         RGPI.have_vertical_velocity = gps.have_vertical_velocity(i);
 
-        float tmp = 0;
-        RGPI.horizontal_accuracy_returncode = gps.horizontal_accuracy(i, tmp);
-        RGPI.hacc = tmp;
-        RGPI.vertical_accuracy_returncode = gps.vertical_accuracy(i, tmp);
-        RGPI.vacc = tmp;
+        RGPI.horizontal_accuracy_returncode = gps.horizontal_accuracy(i, RGPI.hacc);
+        RGPI.vertical_accuracy_returncode = gps.vertical_accuracy(i, RGPI.vacc);
         RGPI.hdop = gps.get_hdop(i);
         RGPI.num_sats = gps.num_sats(i);
-        RGPI.get_lag_returncode = gps.get_lag(i, tmp);
-        RGPI.lag_sec = tmp;
+        RGPI.get_lag_returncode = gps.get_lag(i, RGPI.lag_sec);
         WRITE_REPLAY_BLOCK(RGPI, RGPI);
 
         log_RGPJ &RGPJ = _RGPJ[i];
 
         RGPJ.velocity = gps.velocity(i);
-        RGPJ.speed_accuracy_returncode = gps.speed_accuracy(i, tmp);
-        RGPJ.sacc = tmp;
-        float tmp2 = 0;
-        RGPJ.gps_yaw_deg_returncode = gps.gps_yaw_deg(i, tmp, tmp2);
-        RGPJ.yaw_deg = tmp;
-        RGPJ.yaw_accuracy_deg = tmp2;
+        RGPJ.speed_accuracy_returncode = gps.speed_accuracy(i, RGPJ.sacc);
+        RGPJ.gps_yaw_deg_returncode = gps.gps_yaw_deg(i, RGPJ.yaw_deg, RGPJ.yaw_accuracy_deg);
         RGPJ.antenna_offset = gps.get_antenna_offset(i);
         WRITE_REPLAY_BLOCK(RGPJ, RGPJ);
     }
