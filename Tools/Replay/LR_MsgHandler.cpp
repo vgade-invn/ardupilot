883ad1f679690477af63ba887ff71ed1c572b25c
Andrew Tridgell <andrew@tridgell.net>
2020-10-31 12:23:06 +1100

    Tools: added RVOH handler

--- Tools/Replay/LR_MsgHandler.cpp
+++ Tools/Replay/LR_MsgHandler.cpp
@@ -262,7 +262,7 @@ void LR_MsgHandler_RBCI::process_message(uint8_t *msg)
 
 void LR_MsgHandler_RVOH::process_message(uint8_t *msg)
 {
-    AP::dal().handle_message(*((log_RVOH*)(msg)));
+    AP::dal().handle_message(MSG_CAST(RVOH,msg));
 }
 
 #include <AP_AHRS/AP_AHRS.h>
