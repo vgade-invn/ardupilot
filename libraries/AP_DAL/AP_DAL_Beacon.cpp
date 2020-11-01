5767b23c1b294fb25d0106b790bd23cf2a4e5eaf
Andrew Tridgell <andrew@tridgell.net>
2020-11-02 10:28:55 +1100

    AP_DAL: de-dup beacon and compass

--- libraries/AP_DAL/AP_DAL_Beacon.cpp
+++ libraries/AP_DAL/AP_DAL_Beacon.cpp
@@ -16,6 +16,7 @@ void AP_DAL_Beacon::start_frame(const uint64_t time_us)
     const auto *beacon = AP::beacon();
 
     _RBCH.ptr_is_nullptr = (beacon == nullptr);
+    const log_RBCH old = _RBCH;
     if (beacon != nullptr) {
         _RBCH.count = beacon->count();
         _RBCH.get_vehicle_position_ned_returncode = beacon->get_vehicle_position_ned(_RBCH.vehicle_position_ned, _RBCH.accuracy_estimate);
@@ -25,13 +26,16 @@ void AP_DAL_Beacon::start_frame(const uint64_t time_us)
         _RBCH.origin_lng = _origin.lng;
         _RBCH.origin_alt = _origin.alt;
     }
-    WRITE_REPLAY_BLOCK(RBCH, _RBCH);
+    if (STRUCT_NEQ(old, _RBCH)) {
+        WRITE_REPLAY_BLOCK(RBCH, _RBCH);
+    }
     if (beacon == nullptr) {
         return;
     }
 
     for (uint8_t i=0; i<ARRAY_SIZE(_RBCI); i++) {
         log_RBCI &RBCI = _RBCI[i];
+        const log_RBCI old_RBCI = RBCI;
         const uint32_t last_update_ms = beacon->beacon_last_update_ms(i);
         if (last_update_ms == _last_logged_update_ms[i]) {
             continue;
@@ -42,6 +46,8 @@ void AP_DAL_Beacon::start_frame(const uint64_t time_us)
         RBCI.distance = beacon->beacon_distance(i);
         RBCI.healthy = beacon->beacon_healthy(i);
 
-        WRITE_REPLAY_BLOCK(RBCI, RBCI);
+        if (STRUCT_NEQ(old_RBCI, RBCI)) {
+            WRITE_REPLAY_BLOCK(RBCI, RBCI);
+        }
     }
 }
--- libraries/AP_DAL/AP_DAL_Compass.cpp
+++ libraries/AP_DAL/AP_DAL_Compass.cpp
@@ -15,16 +15,20 @@ void AP_DAL_Compass::start_frame(const uint64_t time_us)
 {
     const auto &compass = AP::compass();
 
+    const log_RMGH old = _RMGH;
     _RMGH.count = compass.get_count();
     _RMGH.auto_declination_enabled = compass.auto_declination_enabled();
     _RMGH.declination = compass.get_declination();
     _RMGH.num_enabled = compass.get_num_enabled();
     _RMGH.consistent = compass.consistent();
 
-    WRITE_REPLAY_BLOCK(RMGH, _RMGH);
+    if (STRUCT_NEQ(old, _RMGH)) {
+        WRITE_REPLAY_BLOCK(RMGH, _RMGH);
+    }
 
     for (uint8_t i=0; i<ARRAY_SIZE(_RMGI); i++) {
         log_RMGI &RMGI = _RMGI[i];
+        const log_RMGI old_RMGI = RMGI;
         const uint32_t last_update_usec = compass.last_update_usec(i);
         if (last_update_usec == _last_logged_update_usec[i]) {
             continue;
@@ -37,6 +41,8 @@ void AP_DAL_Compass::start_frame(const uint64_t time_us)
         RMGI.last_update_usec = last_update_usec;
         memcpy((void*)&RMGI.field, (void*)&compass.get_field(i), sizeof(Vector3f));
 
-        WRITE_REPLAY_BLOCK(RMGI, RMGI);
+        if (STRUCT_NEQ(old_RMGI, _RMGI)) {
+            WRITE_REPLAY_BLOCK(RMGI, RMGI);
+        }
     }
 }
