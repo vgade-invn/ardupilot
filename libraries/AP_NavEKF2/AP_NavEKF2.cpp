ad18fe18c409605242a3b0bea0b88f6fd6042071
Andrew Tridgell <andrew@tridgell.net>
2020-11-02 12:39:32 +1100

    AP_NavEKF2: fixed ins timestamp usage

--- libraries/AP_NavEKF2/AP_NavEKF2.cpp
+++ libraries/AP_NavEKF2/AP_NavEKF2.cpp
@@ -756,8 +756,6 @@ void NavEKF2::UpdateFilter(void)
 
     imuSampleTime_us = AP::dal().micros64();
     
-    const auto &ins = AP::dal().ins();
-
     bool statePredictEnabled[num_cores];
     for (uint8_t i=0; i<num_cores; i++) {
         // if we have not overrun by more than 3 IMU frames, and we
@@ -765,7 +763,7 @@ void NavEKF2::UpdateFilter(void)
         // loop then suppress the prediction step. This allows
         // multiple EKF instances to cooperate on scheduling
         if (core[i].getFramesSincePredict() < (_framesPerPrediction+3) &&
-            (AP::dal().micros() - ins.get_last_update_usec()) > _frameTimeUsec/3) {
+            (AP::dal().micros64() - imuSampleTime_us) > _frameTimeUsec/3) {
             statePredictEnabled[i] = false;
         } else {
             statePredictEnabled[i] = true;
