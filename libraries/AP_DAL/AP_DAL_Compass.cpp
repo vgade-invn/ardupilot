d9565014ed1fdab7aa05dcd222caf9dc5bcaa988
Andrew Tridgell <andrew@tridgell.net>
2020-11-06 10:22:38 +1100

    AP_DAL: added data access layer library
    This provides a wrapper around sensor and system calls to allow for
    logging for replay in EKF2 and EKF3
    
    Co-authored-by: Peter Barker <pbarker@barker.dropbear.id.au>

---
+++ libraries/AP_DAL/AP_DAL_Compass.cpp
@@ -0,0 +1,45 @@
+#include "AP_DAL_Compass.h"
+
+#include <AP_Compass/AP_Compass.h>
+
+#include <AP_Logger/AP_Logger.h>
+#include "AP_DAL.h"
+
+AP_DAL_Compass::AP_DAL_Compass()
+{
+    for (uint8_t i=0; i<ARRAY_SIZE(_RMGI); i++) {
+        _RMGI[i].instance = i;
+    }
+}
+
+void AP_DAL_Compass::start_frame()
+{
+    const auto &compass = AP::compass();
+
+    const log_RMGH old = _RMGH;
+    _RMGH.count = compass.get_count();
+    _RMGH.auto_declination_enabled = compass.auto_declination_enabled();
+    _RMGH.declination = compass.get_declination();
+    _RMGH.num_enabled = compass.get_num_enabled();
+    _RMGH.consistent = compass.consistent();
+
+    WRITE_REPLAY_BLOCK_IFCHANGD(RMGH, _RMGH, old);
+
+    for (uint8_t i=0; i<ARRAY_SIZE(_RMGI); i++) {
+        log_RMGI &RMGI = _RMGI[i];
+        const log_RMGI old_RMGI = RMGI;
+        const uint32_t last_update_usec = compass.last_update_usec(i);
+        if (last_update_usec == _last_logged_update_usec[i]) {
+            continue;
+        }
+        _last_logged_update_usec[i] = last_update_usec;
+        RMGI.use_for_yaw = compass.use_for_yaw(i);
+        RMGI.healthy = compass.healthy(i);
+        memcpy((void*)&RMGI.offsets, (void*)&compass.get_offsets(i), sizeof(Vector3f));
+        RMGI.have_scale_factor = compass.have_scale_factor(i);
+        RMGI.last_update_usec = last_update_usec;
+        memcpy((void*)&RMGI.field, (void*)&compass.get_field(i), sizeof(Vector3f));
+
+        WRITE_REPLAY_BLOCK_IFCHANGD(RMGI, RMGI, old_RMGI);
+    }
+}
