32d73538703260e16673eea9500c68e483bc8de2
Andrew Tridgell <andrew@tridgell.net>
2020-12-13 09:10:33 +1100

    AP_AHRS: fixed build on boards without EKF2

--- libraries/AP_AHRS/AP_AHRS_NavEKF.cpp
+++ libraries/AP_AHRS/AP_AHRS_NavEKF.cpp
@@ -589,14 +589,17 @@ bool AP_AHRS_NavEKF::airspeed_estimate(float &airspeed_ret) const
         break;
 #endif
 
+#if HAL_NAVEKF2_AVAILABLE
     case EKFType::TWO:
         ret = EKF2.getWind(-1,wind_vel);
         break;
+#endif
 
+#if HAL_NAVEKF3_AVAILABLE
     case EKFType::THREE:
         ret = EKF3.getWind(-1,wind_vel);
         break;
-
+#endif
     }
 
     // estimate it via nav velocity and wind estimates
