b405435dfca326a96e1a10c76fa818737cdace12
Andrew Tridgell <andrew@tridgell.net>
2020-12-12 13:37:41 +1100

    RC_Channel: added Q_ASSIST option from master

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -175,6 +175,7 @@ public:
         SURFACE_TRACKING =    75, // Surface tracking upwards or downwards
         STANDBY  =            76, // Standby mode
         TAKEOFF   =           77, // takeoff
+        Q_ASSIST =            82, // disable, enable and force Q assist
         KILL_IMU1 =          100, // disable first IMU (for IMU failure testing)
         KILL_IMU2 =          101, // disable second IMU (for IMU failure testing)
         CAM_MODE_TOGGLE =    102, // Momentary switch to cycle camera modes
