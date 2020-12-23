4e8cc8b8b9a89d6c608b58d050014aa2daf53a29
Paul Riseborough <gncsolns@gmail.com>
2020-12-23 16:35:08 +1100

    Plane: Use adjusted radius for loiter entry calculation

--- ArduPlane/navigation.cpp
+++ ArduPlane/navigation.cpp
@@ -291,7 +291,7 @@ void Plane::update_loiter(uint16_t radius)
     } else if ((loiter.start_time_ms == 0 &&
                 (control_mode == &mode_auto || control_mode == &mode_guided) &&
                 auto_state.crosstrack &&
-                current_loc.get_distance(next_WP_loc) > radius*3) ||
+                current_loc.get_distance(next_WP_loc) > 3 * nav_controller->loiter_radius(fabsf(radius))) ||
                (control_mode == &mode_rtl && quadplane.available() && quadplane.rtl_mode == QuadPlane::RTL_MODE::SWITCH_QRTL)) {
         /*
           if never reached loiter point and using crosstrack and somewhat far away from loiter point
