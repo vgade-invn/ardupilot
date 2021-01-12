2781512d5ce2bfeb412e42ceadffd4cd526ccc55
Andrew Tridgell <andrew@tridgell.net>
2021-01-12 12:07:07 +1100

    Glider: added filter script

---
+++ ArduPlane/Glider/graphs/filter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for f in $*; do
+    bname=$(basename $f .bin)
+    dname=$(dirname $f)
+    newname=$dname/$bname-glide.bin
+    echo Creating $newname
+    mavlogdump.py -q -o "$newname" "$f" --condition='in_mission(GPS,BARO,RCOU)' --nottypes 'NK*,XK*'
+done
