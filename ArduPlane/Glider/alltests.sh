cd678163d63039de35bf8553d5779c014612a127
Andrew Tridgell <andrew@tridgell.net>
2021-01-22 14:50:21 +1100

    Glider: adjust tests script

--- ArduPlane/Glider/alltests.sh
+++ ArduPlane/Glider/alltests.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 rm -rf logs
-./runtest.py mission1 --location KEDW2
+./runtest.py mission1 --location KEDW1
 ./runtest.py mission2 --location KEDW2
-./runtest.py mission3 --location KEDW2
-./runtest.py mission4 --location KEDW2
-./runtest.py mission5 --location KEDW3 --speed-scheduling
-./runtest.py mission6 --location KEDW4 --speed-scheduling
-./runtest.py mission7 --location KEDW5 --speed-scheduling
-./runtest.py mission8 --location KEDW6 --speed-scheduling
-./runtest.py mission9 --location KEDW6 --speed-scheduling
+./runtest.py mission3 --location KEDW3
+./runtest.py mission4 --location KEDW4
+./runtest.py mission5 --location KEDW5 --speed-scheduling
+./runtest.py mission6 --location KEDW6 --speed-scheduling
+./runtest.py mission7 --location KEDW7 --speed-scheduling
+./runtest.py mission8 --location KEDW8 --speed-scheduling
+./runtest.py mission9 --location KEDW9 --speed-scheduling
 ./graphs/filter.sh test_runs/mission?.bin
 ./graphs/graph_logs.py test_runs/mission?-glide.bin
