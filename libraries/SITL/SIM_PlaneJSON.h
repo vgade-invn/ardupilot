aefe46f8a8f1270440677c1ca77b3fbe652bb917
Andrew Tridgell <andrew@tridgell.net>
2021-02-02 08:07:36 +1100

    SITL: fixed sign issue in rudder

--- libraries/SITL/SIM_PlaneJSON.h
+++ libraries/SITL/SIM_PlaneJSON.h
@@ -127,7 +127,7 @@ protected:
         float deltaCnperRadianElev = 0;
 
         // rudder
-        float rudderDeflectionLimitDeg = -18.0;
+        float rudderDeflectionLimitDeg = 18.0;
         float deltaCNperRadianRud = 0;
         float deltaCAperRadianRud = 0.058;
         float deltaCmperRadianRud = 0;
