d3b2544ae240c69521cd32145126b96a65bc6b69
Paul Riseborough <gncsolns@gmail.com>
2021-02-07 10:43:04 +1100

    Plane: Add param files for missions 2 to 8

--- ArduPlane/Glider/alltests.sh
+++ ArduPlane/Glider/alltests.sh
@@ -2,14 +2,14 @@
 
 rm -rf logs
 ./runtest.py mission1 --location KEDW1 --param-file glider-pullup-mission1.parm
-./runtest.py mission2 --location KEDW2 --param-file glider-pullup.parm
-./runtest.py mission3 --location KEDW3 --param-file glider-pullup.parm
-./runtest.py mission4 --location KEDW4 --param-file glider-pullup.parm
-./runtest.py mission5 --location KEDW5 --speed-scheduling --param-file glider-pullup.parm
-./runtest.py mission6 --location KEDW6 --speed-scheduling --param-file glider-pullup.parm
-./runtest.py mission7 --location KEDW7 --speed-scheduling --param-file glider-pullup.parm
-./runtest.py mission8 --location KEDW8 --speed-scheduling --param-file glider-pullup.parm
-./runtest.py mission9 --location KEDW9 --speed-scheduling --param-file glider-pullup-mision9.parm
+./runtest.py mission2 --location KEDW2 --param-file glider-pullup-mission2.parm
+./runtest.py mission3 --location KEDW3 --param-file glider-pullup-mission3.parm
+./runtest.py mission4 --location KEDW4 --param-file glider-pullup-mission4.parm
+./runtest.py mission5 --location KEDW5 --speed-scheduling --param-file glider-pullup-mission5.parm
+./runtest.py mission6 --location KEDW6 --speed-scheduling --param-file glider-pullup-mission6.parm
+./runtest.py mission7 --location KEDW7 --speed-scheduling --param-file glider-pullup-mission7.parm
+./runtest.py mission8 --location KEDW8 --speed-scheduling --param-file glider-pullup-mission8.parm
+./runtest.py mission9 --location KEDW9 --speed-scheduling --param-file glider-pullup-mission9.parm
 for i in $(seq 1 9); do
     (./graphs/filter.sh test_runs/mission$i.bin && ./graphs/graph_logs.py test_runs/mission$i-glide.bin) &
 done
