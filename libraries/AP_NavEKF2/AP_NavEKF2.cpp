09ef8294deaf9b1cb8306bd1c0a7a6f40eac9f52
Andrew Tridgell <andrew@tridgell.net>
2021-02-13 21:41:36 +1100

    AP_NavEKF2: expose current errorScore

--- libraries/AP_NavEKF2/AP_NavEKF2.cpp
+++ libraries/AP_NavEKF2/AP_NavEKF2.cpp
@@ -1653,3 +1653,9 @@ void NavEKF2::writeExtNavVelData(const Vector3f &vel, float err, uint32_t timeSt
         }
     }
 }
+
+// return error score for currently active lane
+float NavEKF2::errorScore(void) const
+{
+    return core[primary].errorScore();
+}
