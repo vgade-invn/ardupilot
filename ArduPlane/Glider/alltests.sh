ddbacc6558a7dd9313868b7b466f7cf461d2df4b
Andrew Tridgell <andrew@tridgell.net>
2021-02-18 08:56:03 +1100

    Glider: make testing more parallel

--- ArduPlane/Glider/alltests.sh
+++ ArduPlane/Glider/alltests.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
 rm -rf logs
-./runtest.py --mission 1
-./runtest.py --mission 2
-./runtest.py --mission 3
-./runtest.py --mission 4
-./runtest.py --mission 5
-./runtest.py --mission 6
-./runtest.py --mission 7
-./runtest.py --mission 8
-./runtest.py --mission 9
 
 for i in $(seq 1 9); do
-    (./graphs/filter.sh test_runs/mission$i.bin && ./graphs/graph_logs.py test_runs/mission$i-glide.bin) &
+   nice ./runtest.py --mission $i --no-ui
+   (nice ./graphs/filter.sh test_runs/mission$i.bin &&
+    nice ./graphs/graph_logs.py test_runs/mission$i-glide.bin &&
+    nice ./tocsv.sh test_runs/mission$i-glide.bin) &
 done
 wait
 ./graphs/make_index.sh test_runs/mission*glide*bin > test_runs/index.html
-./tocsv.sh test_runs/mission?-glide.bin
