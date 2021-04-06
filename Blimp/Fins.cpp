a8e11ff7f75c1177be4f5eb17c46398d5c1b09cb
Michelle Rossouw <64898873+MichelleRos@users.noreply.github.com>
2021-04-06 16:53:02 +1000

    Blimp: Remove unused parameters

--- Blimp/Fins.cpp
+++ Blimp/Fins.cpp
@@ -1,6 +1,6 @@
 #include "Blimp.h"
 
-// This is the scale used for RC inputs so that they can be scaled to the float point values used in the sin wave code.
+// This is the scale used for RC inputs so that they can be scaled to the float point values used in the sine wave code.
 #define FIN_SCALE_MAX 1000
 
 /*
