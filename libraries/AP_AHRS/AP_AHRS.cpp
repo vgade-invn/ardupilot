0ca5e53897e004d2b0bd93e1beea8e8552d31385
Andrew Tridgell <andrew@tridgell.net>
2021-04-14 20:50:38 +1000

    AP_AHRS: added lock_position API

--- libraries/AP_AHRS/AP_AHRS.cpp
+++ libraries/AP_AHRS/AP_AHRS.cpp
@@ -3090,6 +3090,16 @@ const EKFGSF_yaw *AP_AHRS::get_yaw_estimator(void) const
     return nullptr;
 }
 
+// lock position for inertial takeoff
+bool AP_AHRS_NavEKF::lock_position(bool enable)
+{
+#if HAL_NAVEKF3_AVAILABLE
+    return EKF3.lockPosition(enable);
+#else
+    return false;
+#endif
+}
+
 // singleton instance
 AP_AHRS *AP_AHRS::_singleton;
 
