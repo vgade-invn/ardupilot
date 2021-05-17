b1c2d6b64746458d1dae0465164a8ae59331b611
Andrew Tridgell <andrew@tridgell.net>
2021-05-18 08:38:34 +1000

    AP_Vehicle: added fence_distance_inside for lua

--- libraries/AP_Vehicle/AP_Vehicle.h
+++ libraries/AP_Vehicle/AP_Vehicle.h
@@ -160,6 +160,7 @@ public:
      */
     virtual bool enable_fence(void) { return false; }
     virtual bool fence_enabled(void) { return false; }
+    virtual float fence_distance_inside(void) { return -1; }
 
     /*
       get the likely flying status. Returns true if the vehicle code
