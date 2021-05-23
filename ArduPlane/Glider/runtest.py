4b9493a433c64a8fc53c528f0f909bba5e482ecf
Andrew Tridgell <andrew@tridgell.net>
2021-05-23 15:05:00 +1000

    Glider: fix for new balloon rate

--- ArduPlane/Glider/runtest.py
+++ ArduPlane/Glider/runtest.py
@@ -111,7 +111,7 @@ mavproxy.send('arm throttle\n')
 mavproxy.expect('Throttle armed')
 mavproxy.send('auto\n')
 wait_mode(mav, ['AUTO'])
-mavproxy.send('rc 6 1556\n')
+mavproxy.send('rc 6 2000\n')
 if not args.no_ui:
     mavproxy.send('module load map\n')
     mavproxy.send('wp list\n')
