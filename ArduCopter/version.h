67d844ffb0ed621fd51bfdee00a389fd6b3b4ca8
Leonard Hall <leonardthall@gmail.com>
2021-05-28 00:14:12 +0930

    Copter: version to 4.0.7 Cal21c

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.0.7 Cal21b"
+#define THISFIRMWARE "ArduCopter V4.0.7 Cal21c"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,0,7,FIRMWARE_VERSION_TYPE_OFFICIAL
