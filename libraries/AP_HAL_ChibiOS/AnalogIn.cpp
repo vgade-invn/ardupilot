5ffe3593a4bf6b1fdba6ac125aa1c2e1a87c6457
Andrew Tridgell <andrew@tridgell.net>
2021-08-21 10:54:32 +1000

    HAL_ChibiOS: log ADC3 message
    MCUTemp and VRef

--- libraries/AP_HAL_ChibiOS/AnalogIn.cpp
+++ libraries/AP_HAL_ChibiOS/AnalogIn.cpp
@@ -17,6 +17,7 @@
 #include <AP_HAL/AP_HAL.h>
 #include "ch.h"
 #include "hal.h"
+#include <AP_Logger/AP_Logger.h>
 
 #if HAL_USE_ADC == TRUE && !defined(HAL_DISABLE_ADC_DRIVER)
 
@@ -279,7 +280,7 @@ void AnalogIn::init()
 
 
 
-#define ADC3_GRP1_NUM_CHANNELS 3
+#define ADC3_GRP1_NUM_CHANNELS 2
 
 // internal ADC channels
 #define ADC3_VSENSE_CHAN 18
@@ -318,7 +319,6 @@ void AnalogIn::setup_adc3(void)
 
     adcSTM32EnableVREF(&ADCD3);
     adcSTM32EnableTS(&ADCD3);
-    adcSTM32EnableVBAT(&ADCD3);
 
     memset(&adc3grpcfg, 0, sizeof(adc3grpcfg));
     adc3grpcfg.circular = true;
@@ -336,7 +336,7 @@ void AnalogIn::setup_adc3(void)
     adc3grpcfg.cr2 = ADC_CR2_SWSTART;
 #endif
 
-    const uint8_t channels[ADC3_GRP1_NUM_CHANNELS] = { ADC3_VBAT4_CHAN, ADC3_VSENSE_CHAN, ADC3_VREFINT_CHAN };
+    const uint8_t channels[ADC3_GRP1_NUM_CHANNELS] = { ADC3_VSENSE_CHAN, ADC3_VREFINT_CHAN };
 
     for (uint8_t i=0; i<ADC3_GRP1_NUM_CHANNELS; i++) {
         uint8_t chan = channels[i];
@@ -442,29 +442,27 @@ void AnalogIn::_timer_tick(void)
         }
     }
 
-    uint32_t buf_adc3[ADC3_GRP1_NUM_CHANNELS];
+    // 20Hz temperature and ref voltage
+    static uint32_t last_mav_send_us;
+    if (now - last_mav_send_us > 50000 &&
+        hal.scheduler->is_system_initialized()) {
+        last_mav_send_us = now;
 
-    /* read all channels available */
-    read_adc3(buf_adc3);
+        uint32_t buf_adc3[ADC3_GRP1_NUM_CHANNELS];
+        read_adc3(buf_adc3);
 
-    const float TS_CAL1 = *(const volatile uint16_t *)0x1FF1E820;
-    const float TS_CAL2 = *(const volatile uint16_t *)0x1FF1E840;
+        const float TS_CAL1 = *(const volatile uint16_t *)0x1FF1E820;
+        const float TS_CAL2 = *(const volatile uint16_t *)0x1FF1E840;
+        const float VREFINT_CAL = *(const volatile uint16_t *)0x1FF1E860;
 
-    const float mcu_temp = ((110 - 30) / (TS_CAL2 - TS_CAL1)) * (float(buf_adc3[1]) - TS_CAL1) + 30;
-    
-    static uint8_t count;
-    if (AP_HAL::millis() > 5000 && count++ == 10) {
-        count = 0;
-        uint16_t adc[6] {};
-        uint8_t n = ADC3_GRP1_NUM_CHANNELS;
-        if (n > 6) {
-            n = 6;
-        }
-        for (uint8_t i=0; i < n; i++) {
-            adc[i] = buf_adc3[i];
-        }
-        adc[5] = mcu_temp * 100;
-        mavlink_msg_ap_adc_send(MAVLINK_COMM_0, adc[0], adc[1], adc[2], adc[3], adc[4], adc[5]);
+        const float mcu_temp = ((110 - 30) / (TS_CAL2 - TS_CAL1)) * (float(buf_adc3[0]) - TS_CAL1) + 30;
+        const float vref = 3.3 * VREFINT_CAL / float(buf_adc3[1]+0.001);
+
+        mavlink_msg_ap_adc_send(MAVLINK_COMM_0, uint16_t(mcu_temp*100), uint16_t(vref*1000), 0, 0, 0, 0);
+        AP::logger().WriteStreaming("ADC3", "TimeUS,MCUTemp,VRef", "Qff",
+                                    AP_HAL::micros64(),
+                                    mcu_temp,
+                                    vref);
     }
 }
 
