4a3b6854429119dd5ebac8e93e517095df35ff34
Andrew Tridgell <andrew@tridgell.net>
2021-10-27 10:26:07 +1100

    AP_Scripting: improve aerobatics example script
    add rudder gain and roll/pitch P controllers

--- libraries/AP_Scripting/examples/plane_aerobatics.lua
+++ libraries/AP_Scripting/examples/plane_aerobatics.lua
@@ -4,6 +4,77 @@ local running = false
 
 local roll_stage = 0
 
+local ROLL_TCONST = param:get('RLL2SRV_TCONST') * 0.5
+local PITCH_TCONST = param:get('PTCH2SRV_TCONST') * 0.5
+local TRIM_THROTTLE = param:get('TRIM_THROTTLE') * 1.0
+
+local scr_user1_param = Parameter()
+local scr_user2_param = Parameter()
+local scr_user3_param = Parameter()
+assert(scr_user1_param:init('SCR_USER1'), 'could not find SCR_USER1 parameter')
+assert(scr_user2_param:init('SCR_USER2'), 'could not find SCR_USER2 parameter')
+assert(scr_user3_param:init('SCR_USER3'), 'could not find SCR_USER3 parameter')
+
+local last_roll_err = 0.0
+
+-- find our rudder channel
+local RUDDER_CHAN = SRV_Channels:find_channel(21)
+local RUDDER_TRIM = param:get("SERVO" .. RUDDER_CHAN + 1 .. "_TRIM")
+local RUDDER_REVERSED = param:get("SERVO" .. RUDDER_CHAN + 1 .. "_REVERSED")
+local RUDDER_MIN = param:get("SERVO" .. RUDDER_CHAN + 1 .. "_MIN")
+local RUDDER_MAX = param:get("SERVO" .. RUDDER_CHAN + 1 .. "_MAX")
+local RUDDER_THROW = (RUDDER_MAX - RUDDER_MIN) * 0.5
+
+-- constrain a value between limits
+function constrain(v, vmin, vmax)
+   if v < vmin then
+      v = vmin
+   end
+   if v > vmax then
+      v = vmax
+   end
+   return v
+end
+
+-- a controller to target a zero roll angle, coping with inverted flight
+-- output is a body frame roll rate, with convergence over time tconst in seconds
+function roll_zero_controller(tconst)
+   local roll_deg = math.deg(ahrs:get_roll())
+   local pitch_deg = math.deg(ahrs:get_pitch())
+   local roll_err = 0.0
+   if math.abs(pitch_deg) > 85 then
+      -- close to 90 we retain the last roll rate
+      roll_err = last_roll_err
+   elseif roll_deg > 90 then
+      roll_err = 180 - roll_deg
+   elseif roll_deg < -90 then
+      roll_err = (-180) - roll_deg
+   else
+      roll_err = -roll_deg
+   end
+   last_roll_err = roll_err
+   return roll_err / tconst
+end
+
+-- a controller to target a zero pitch angle
+-- output is a body frame pitch rate, with convergence over time tconst in seconds
+function pitch_controller(target_pitch_deg, tconst)
+   local roll_deg = math.deg(ahrs:get_roll())
+   local pitch_deg = math.deg(ahrs:get_pitch())
+   local pitch_rate = (target_pitch_deg - pitch_deg) * math.cos(math.rad(roll_deg)) / tconst
+   RUDDER_GAIN = scr_user1_param:get()
+   local rudder = pitch_deg * RUDDER_GAIN * math.sin(math.rad(roll_deg)) / tconst
+   return pitch_rate, rudder
+end
+
+-- a controller for throttle to account for pitch
+function throttle_controller(tconst)
+   local pitch_rad = ahrs:get_pitch()
+   local thr_ff = scr_user3_param:get()
+   local throttle = TRIM_THROTTLE + math.sin(pitch_rad) * thr_ff
+   return constrain(throttle, 0.0, 100.0)
+end
+
 function do_axial_roll(m)
    -- constant roll rate axial roll
    if not running then
@@ -30,7 +101,15 @@ function do_axial_roll(m)
       end
    end
    if roll_stage < 2 then
-      vehicle:nav_scripting_update(false, throttle, roll_rate, 0, 0)
+      target_pitch = scr_user2_param:get()
+      pitch_rate, rudder = pitch_controller(target_pitch, PITCH_TCONST)
+      vehicle:nav_scripting_update(false, throttle, roll_rate, pitch_rate, 0)
+      if RUDDER_REVERSED == 1 then
+         rudder = -rudder
+      end
+      local rudder_out = math.floor(RUDDER_TRIM + RUDDER_THROW * rudder)
+      rudder_out = constrain(rudder_out, RUDDER_MIN, RUDDER_MAX)
+      SRV_Channels:set_output_pwm_chan_timeout(RUDDER_CHAN, rudder_out, 50)
    end
 end
 
@@ -44,7 +123,7 @@ function do_loop(m)
       gcs:send_text(0, string.format("Starting loop"))
    end
    local pitch_rate = m:param3()
-   local throttle = m:param4()
+   local throttle = throttle_controller()
    local pitch_deg = math.deg(ahrs:get_pitch())
    local roll_deg = math.deg(ahrs:get_roll())
    if loop_stage == 0 then
@@ -62,7 +141,8 @@ function do_loop(m)
       end
    end
    if loop_stage < 2 then
-      vehicle:nav_scripting_update(false, throttle, 0, pitch_rate, 0)
+      local roll_rate = roll_zero_controller(ROLL_TCONST)
+      vehicle:nav_scripting_update(false, throttle, roll_rate, pitch_rate, 0)
    end
 end
 
