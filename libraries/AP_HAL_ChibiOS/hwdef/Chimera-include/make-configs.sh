e2f39a7b1feffa646b886544a03fb9dea4a4fda8
Peter Barker <pbarker@barker.dropbear.id.au>
2022-04-29 09:54:03 +1000

    hwdef: config for Spektrum/SharkByte

--- libraries/AP_HAL_ChibiOS/hwdef/Chimera-include/make-configs.sh
+++ libraries/AP_HAL_ChibiOS/hwdef/Chimera-include/make-configs.sh
@@ -5,3 +5,5 @@ set -x
 
 cat tune.parm config.parm DJI.parm >../Chimera-DJI/defaults.parm
 cat tune.parm config.parm Crossfire.parm >../Chimera-XFire-Ana/defaults.parm
+
+cat tune.parm config.parm SharkByte.parm Spektrum.parm >../Chimera-Spektrum-SharkByte/defaults.parm
