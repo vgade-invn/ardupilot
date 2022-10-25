565df404c09dc2a1f2d26b554029bba89ef50e19
Jono453 <jonathan.loong9328@gmail.com>
2022-10-24 22:46:31 -0700

    Version & Release update Carbopilot 4.1.2_ANU_V1, CarboPeriph 4.1_ANU_V1

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V4.2.1_Carbopilot V4.1.1"
+#define THISFIRMWARE "ArduPlane V4.2.1_Carbopilot V4.1.2_ANU_V1"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,2,1,FIRMWARE_VERSION_TYPE_OFFICIAL
