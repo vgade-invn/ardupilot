457aab593a3158b257bbd51aadea1243ea202f07
Lokesh Ramina <lokesh@carbonix.com.au>
2023-02-01 11:15:21 +1100

    OV3-399 Ottano: 4.2.5-beta release notes and version

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "Ottano Carbopilot V4.2.4"
+#define THISFIRMWARE "Ottano Carbopilot V4.2.5"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,2,1,FIRMWARE_VERSION_TYPE_OFFICIAL
