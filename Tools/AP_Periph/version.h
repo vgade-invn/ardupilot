af2d90cf4e5b1cf51ba35dde91cd5bcfd5fa403b
Lokesh Ramina <lokesh@carbonix.com.au>
2023-02-01 12:33:33 +1100

    OV3-404 AP_Periph: version number in header
    From now we don't need to edit the print command in AP_Periph .cpp

--- Tools/AP_Periph/version.h
+++ Tools/AP_Periph/version.h
@@ -1,14 +1,18 @@
 #pragma once
 
-#define THISFIRMWARE "AP_Periph V1.3dev_CarboPeriph V4.0"
+#ifndef __AP_PERIPH_FW_VERSION__
+#define __AP_PERIPH_FW_VERSION__
+
+#define THISFIRMWARE "Ottano Carbopilot V4.2.1 OV3-404"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 1,3,0,FIRMWARE_VERSION_TYPE_DEV
 
+#ifndef FW_MAJOR
 #define FW_MAJOR 1
 #define FW_MINOR 3
 #define FW_PATCH 0
 #define FW_TYPE FIRMWARE_VERSION_TYPE_DEV
+#endif
 
-
-
+#endif
