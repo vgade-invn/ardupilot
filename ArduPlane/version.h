166167f247610389e725f0d0f84d377ddd90a347
Pradeep CK <ckpradip@gmail.com>
2023-02-01 15:09:05 +1100

    OV3-443 Ottano: 4.3.1-beta release notes and version

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "Ottano Carbopilot V4.3.0"
+#define THISFIRMWARE "Ottano Carbopilot V4.3.1"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,2,1,FIRMWARE_VERSION_TYPE_OFFICIAL
