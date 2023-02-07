1083a5912333b4b1597ff76330ae6d1549fc18ce
Andrew Tridgell <andrew@tridgell.net>
2023-02-07 18:45:53 +1100

    Plane: use lower roll limit for turn threshold
    slow turn needs earlier turn start

--- ArduPlane/commands_logic.cpp
+++ ArduPlane/commands_logic.cpp
@@ -648,7 +648,7 @@ bool Plane::verify_nav_wp(const AP_Mission::Mission_Command& cmd)
         acceptance_distance_m = cmd_acceptance_distance;
     } else if (cmd_passby == 0) {
         float roll_limit = is_positive(g2.lim_roll_auto)?g2.lim_roll_auto:aparm.roll_limit_cd*0.01;
-        acceptance_distance_m = nav_controller->turn_distance(g.waypoint_radius, auto_state.next_turn_angle, roll_limit);
+        acceptance_distance_m = nav_controller->turn_distance(g.waypoint_radius, auto_state.next_turn_angle, roll_limit*0.75);
     } else {
 
     }
