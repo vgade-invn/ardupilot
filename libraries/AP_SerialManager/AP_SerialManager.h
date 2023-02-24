54c6520969ccb62246a99bcde3c71c18da047ac7
Ryan Friedman <ryanfriedman5410+github@gmail.com>
2023-02-24 08:17:15 -0700

    Rename serial protocol to match the official procotol name
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>

--- libraries/AP_SerialManager/AP_SerialManager.h
+++ libraries/AP_SerialManager/AP_SerialManager.h
@@ -169,7 +169,7 @@ public:
         SerialProtocol_MSP_DisplayPort = 42,
         SerialProtocol_MAVLinkHL = 43,
         SerialProtocol_Tramp = 44,
-        SerialProtocol_ROS2 = 45,
+        SerialProtocol_DDS_XRCE = 45,
         SerialProtocol_NumProtocols                    // must be the last value
     };
 
