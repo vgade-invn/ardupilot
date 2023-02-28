57903346e380ec7382f19b893594a34c1b7c8909
Ryan Friedman <ryanfriedman5410+github@gmail.com>
2023-02-27 22:50:58 -0700

    AP_XRCE_Client: Remove rest of XML and unused middle layer
    * XRCE_Generic_Topic is now largely unused, it's only doing allocations of topics
    
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>

--- libraries/AP_XRCE_Client/AP_XRCE_Topic.cpp
+++ libraries/AP_XRCE_Client/AP_XRCE_Topic.cpp
@@ -7,53 +7,6 @@
 
 XRCE_Generic_Topic::XRCE_Generic_Topic() 
 {
-    particpant_name = (char*)"Ardupilot_DDS_XRCE_Client";
-}
-
-char* XRCE_Generic_Topic::get_participant_name()
-{
-    return particpant_name;
-}
-
-char* XRCE_Generic_Topic::get_topic_name()
-{
-    return topic_name;
-}
-
-char* XRCE_Generic_Topic::get_datatype_name()
-{
-    return datatype_name;
-}
-
-bool XRCE_Generic_Topic::uros_initialize(const char* packageName)
-{
-    ExpandingString *temptopic = new ExpandingString();
-    temptopic->printf("rt/");
-    if(topic_name != nullptr && !temptopic->has_failed_allocation()){
-        if(temptopic->append(topic_name,strlen(topic_name))){
-            topic_name = temptopic->get_writeable_string();
-        } else {
-            return false;
-        }
-    } else {
-        return false;
-    }
-    ExpandingString *tempdtype = new ExpandingString();
-    tempdtype->printf("%s::msg::dds_::", packageName);
-    if(datatype_name != nullptr && !tempdtype->has_failed_allocation()){
-        if(tempdtype->append(datatype_name,strlen(datatype_name))){
-            if(tempdtype->append("_",(uint32_t)strlen("_"))){
-                datatype_name = tempdtype->get_writeable_string();
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    } else {
-        return false;
-    }
 }
 
 XRCE_Generic_Topic* set_topic_instance(uint16_t topic_key)
--- libraries/AP_XRCE_Client/AP_XRCE_Topic.h
+++ libraries/AP_XRCE_Client/AP_XRCE_Topic.h
@@ -28,21 +28,11 @@ class XRCE_Generic_Topic {
 public:
 
     XRCE_Generic_Topic();
-    virtual bool topic_initialize(uint8_t xrce_type) = 0;
     virtual bool serialize_topic(ucdrBuffer *writer) = 0;
     virtual bool deserialize_topic(ucdrBuffer *reader) = 0;
     virtual uint32_t size_of_topic(uint32_t size) = 0;
     virtual void update_topic() = 0;
-    char* get_participant_name();
-    char* get_topic_name();
-    char* get_datatype_name();
 
-protected:
-
-    char* particpant_name;
-    char* topic_name;
-    char* datatype_name;
-    bool uros_initialize(const char* packageName);
 };
 
 XRCE_Generic_Topic* set_topic_instance(uint16_t topic_key);
