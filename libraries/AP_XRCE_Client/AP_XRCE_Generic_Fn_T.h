9c9f60aea48af1cec1e9bf5258d9aa1cfab06cba
Andrew Tridgell <andrew@tridgell.net>
2023-03-01 19:46:18 +1100

    AP_XRCE_Client: build fix

--- libraries/AP_XRCE_Client/AP_XRCE_Generic_Fn_T.h
+++ libraries/AP_XRCE_Client/AP_XRCE_Generic_Fn_T.h
@@ -4,4 +4,4 @@
 // Function pointer that matches signature of generated topics
 typedef bool (*Generic_serialize_topic_fn_t)(struct ucdrBuffer*, const void*);
 typedef bool (*Generic_deserialize_topic_fn_t)(struct ucdrBuffer* , void*);
-typedef bool (*Generic_size_of_topic_fn_t)(struct ucdrBuffer*, uint32_t);
+typedef uint32_t (*Generic_size_of_topic_fn_t)(struct ucdrBuffer*, uint32_t);
