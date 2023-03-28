ae9c6807569c793f20c7d12c5a21c1a81b20e7c7
bugobliterator <siddharthbharatpurohit@gmail.com>
2023-03-28 16:46:03 +1100

    AP_Networking: replace AP_Networking_Serial2UDP with AP_Networking_Serial

---
+++ libraries/AP_Networking/AP_Networking_Serial.cpp
@@ -0,0 +1,140 @@
+#include "AP_Networking_Serial.h"
+#include "AP_Networking.h"
+
+#if AP_SERIAL_EXTENSION_ENABLED
+extern const AP_HAL::HAL& hal;
+
+void AP_Networking_Serial::begin(uint32_t b, uint16_t rxS, uint16_t txS)
+{
+    if (rxS == 0) {
+        rxS = AP_NETWORKING_UDP_RX_BUF_SIZE;
+    }
+    if (txS == 0) {
+        txS = AP_NETWORKING_UDP_TX_BUF_SIZE;
+    }
+    // initialise bytebuffers
+    _readbuf.set_size(rxS);
+    _writebuf.set_size(txS);
+
+    // setup UDP socket
+    if (pcb == nullptr) {
+        pcb = udp_new_ip_type(IPADDR_TYPE_ANY);
+        if (pcb != nullptr) {
+            // allow to sending broadcast packets
+            ip_set_option(pcb, SOF_BROADCAST);
+            // set up RX callback
+            udp_recv(pcb, AP_Networking_Serial::udp_recv_callback, this);
+        }
+    }
+
+    // start a thread to send data
+    if (thread_ctx == nullptr) {
+        // create thread name
+        snprintf(thread_name, sizeof(thread_name), "UDP-%u", dst_port);
+        if(!hal.scheduler->thread_create(FUNCTOR_BIND_MEMBER(&AP_Networking_Serial::update, void), thread_name, 512, AP_HAL::Scheduler::PRIORITY_UART, 0)) {
+            AP_HAL::panic("Failed to create AP_Networking_Serial thread");
+        }
+    }
+    _initialized = true;
+}
+
+void AP_Networking_Serial::udp_recv_callback(void *arg, struct udp_pcb *pcb, struct pbuf *p, const ip_addr_t *addr, u16_t port)
+{
+    AP_Networking_Serial* driver = (AP_Networking_Serial*)arg;
+    if (driver != nullptr)
+    {
+        WITH_SEMAPHORE(driver->_sem);
+        driver->_readbuf.write((uint8_t*)p->payload, p->len);
+    }
+    pbuf_free(p);
+}
+
+void AP_Networking_Serial::end()
+{
+    if (pcb != nullptr) {
+        udp_remove(pcb);
+        pcb = nullptr;
+    }
+}
+
+void AP_Networking_Serial::flush()
+{
+    // nothing to do
+}
+
+void AP_Networking_Serial::set_blocking_writes(bool blocking)
+{
+    blocking_writes = blocking;
+}
+
+bool AP_Networking_Serial::read(uint8_t &b)
+{
+    WITH_SEMAPHORE(_sem);
+    return (_readbuf.read(&b, 1) == 1) ? true : false;
+}
+
+ssize_t AP_Networking_Serial::read(uint8_t *buffer, uint16_t count)
+{
+    WITH_SEMAPHORE(_sem);
+    return _readbuf.read(buffer, count);
+}
+
+size_t AP_Networking_Serial::write(uint8_t c)
+{
+    return write(&c, 1);
+}
+
+size_t AP_Networking_Serial::write(const uint8_t *buffer, size_t size)
+{
+    WITH_SEMAPHORE(_sem);
+    if (pcb == nullptr) {
+        return 0;
+    }
+    if (size == 0) {
+        return 0;
+    }
+    if (blocking_writes) {
+        struct pbuf *p = pbuf_alloc_reference((void*)buffer, size, PBUF_REF);
+        if (p == nullptr) {
+            return 0;
+        }
+        const err_t err = udp_sendto(pcb, p, &dst_addr, dst_port);
+        pbuf_free(p);
+        if (err != ERR_OK) {
+            return 0;
+        }
+    } else {
+        if (_writebuf.space() < size) {
+            return 0;
+        }
+        _writebuf.write(buffer, size);
+    }
+    return size;
+}
+
+void AP_Networking_Serial::update()
+{
+    // send any pending data
+    WITH_SEMAPHORE(_sem);
+    if (pcb == nullptr) {
+        return;
+    }
+    if (_writebuf.available()) {
+        ByteBuffer::IoVec vec[2];
+        const auto n_vec = _writebuf.peekiovec(vec, 2);
+        for (uint8_t i=0; i<n_vec; i++) {
+            struct pbuf *p = pbuf_alloc_reference((void*)vec[i].data, vec[i].len, PBUF_REF);
+            if (p == nullptr) {
+                break;
+            }
+            const err_t err = udp_sendto(pcb, p, &dst_addr, dst_port);
+            pbuf_free(p);
+            if (err != ERR_OK) {
+                break;
+            }
+            _writebuf.advance(vec[i].len);
+        }
+    }
+}
+
+#endif
---
+++ libraries/AP_Networking/AP_Networking_Serial.h
@@ -0,0 +1,85 @@
+#pragma once
+#include <AP_HAL/AP_HAL.h>
+#include <AP_HAL/AP_HAL_Boards.h>
+#include <AP_HAL/Semaphores.h>
+#if AP_SERIAL_EXTENSION_ENABLED || HAL_ENABLE_SERIAL_TUNNEL
+#include <AP_SerialManager/AP_SerialManager.h>
+#include <lwip/udp.h>
+#include <lwip/ip_addr.h>
+
+#ifndef AP_NETWORKING_UDP_RX_BUF_SIZE
+#define AP_NETWORKING_UDP_RX_BUF_SIZE 1024
+#endif
+#ifndef AP_NETWORKING_UDP_TX_BUF_SIZE
+#define AP_NETWORKING_UDP_TX_BUF_SIZE 1024
+#endif
+
+class AP_Networking_Serial : public AP_HAL::UARTDriver {
+public:
+
+    AP_Networking_Serial(AP_SerialManager::SerialExtState::IP_Params &ip_params) :
+        dst_port(ip_params.port)
+    {
+        dst_addr = IPADDR4_INIT_BYTES(ip_params.ip[0], ip_params.ip[1], ip_params.ip[2], ip_params.ip[3]);
+    }
+
+    /* Implementations of UARTDriver virtual methods */
+    void begin(uint32_t b) override {
+        begin(b, 0, 0);
+    }
+    void begin(uint32_t b, uint16_t rxS, uint16_t txS) override;
+
+    void end() override;
+    void flush() override;
+    void set_blocking_writes(bool blocking) override;
+
+    bool is_initialized() override {
+        return _initialized;
+    }
+
+    bool tx_pending() override {
+        return _writebuf.available() > 0;
+    }
+
+    /* Implementations of Stream virtual methods */
+    uint32_t available() override {
+        return _readbuf.available();
+    }
+    uint32_t txspace() override {
+        return _writebuf.space();
+    }
+    bool read(uint8_t &b) override;
+    ssize_t read(uint8_t *buffer, uint16_t count) override;
+
+    bool discard_input() override {
+        _readbuf.clear();
+        return true;
+    }
+
+    /* Implementations of Print virtual methods */
+    size_t write(uint8_t c) override;
+    size_t write(const uint8_t *buffer, size_t size) override;
+
+private:
+    bool _initialized = false;
+
+    ip_addr_t dst_addr;
+    uint16_t dst_port = 0;
+
+    ByteBuffer _readbuf{0};
+    ByteBuffer _writebuf{0};
+
+    HAL_Semaphore _sem;
+
+    struct udp_pcb *pcb;
+    bool blocking_writes = false;
+
+    thread_t* volatile thread_ctx;
+    char thread_name[10];
+
+    uint8_t ins = 0;
+
+    static void udp_recv_callback(void *arg, struct udp_pcb *pcb, struct pbuf *p, const ip_addr_t *addr, u16_t port);
+    void update();
+};
+#endif
