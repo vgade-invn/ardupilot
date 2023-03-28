8d4e1e8ef909c9fa9c031b6dcb81166b400f2107
Tom Pittenger <tom.pittenger@krausaerospace.com>
2023-03-29 10:18:07 +1100

    AP_HAL_ChiboOS: update H743 linker info for Ethernet

--- libraries/AP_HAL_ChibiOS/hwdef/scripts/STM32H743xx.py
+++ libraries/AP_HAL_ChibiOS/hwdef/scripts/STM32H743xx.py
@@ -11,15 +11,8 @@ build = {
 
 # MCU parameters
 mcu = {
-    # DMA peripheral capabilities:
-    # - can't use ITCM or DTCM for any DMA
-    # - SPI1 to SPI5 can use AXI SRAM, SRAM1 to SRAM3 and SRAM4 for DMA
-    # - SPI6, I2C4 and ADC3 can use SRAM4 on BDMA
-    # - UARTS can use AXI SRAM, SRAM1 to SRAM3 and SRAM4 for DMA
-    # - I2C1, I2C2 and I2C3 can use AXI SRAM, SRAM1 to SRAM3 and SRAM4 with DMA
-    # - timers can use AXI SRAM, SRAM1 to SRAM3 and SRAM4 with DMA
-    # - ADC12 can use AXI SRAM, SRAM1 to SRAM3 and SRAM4
-    # - SDMMC can use AXI SRAM, SRAM1 to SRAM3 with IDMA (cannot use SRAM4)
+    # location of MCU serial number
+    'UDID_START' : 0x1FF1E800,
 
     # ram map, as list of (address, size-kb, flags)
     # flags of 1 means DMA-capable (DMA and BDMA)
@@ -30,7 +23,7 @@ mcu = {
         (0x20000000, 128, 2), # DTCM, tightly coupled, no DMA, fast
         (0x24000000, 512, 4), # AXI SRAM.
 		(0x00000400,  63, 2), # ITCM (first 1k removed, to keep address 0 unused)
-        (0x30040000,  32, 0), # SRAM3.
+        #(0x30040000,  32, 0), # SRAM3.
         (0x38000000,  64, 1), # SRAM4.
     ],
 
@@ -39,10 +32,13 @@ mcu = {
         (0x24000000, 512, 4), # AXI SRAM.
         (0x30000000, 256, 0), # SRAM1, SRAM2
         (0x20000000, 128, 2), # DTCM, tightly coupled, no DMA, fast
-        (0x00000400,  63, 2), # ITCM (first 1k removed, to keep address 0 unused)
-        (0x30040000,  32, 0), # SRAM3.
         (0x38000000,  64, 1), # SRAM4.
+        (0x24000000, 512, 4), # AXI SRAM. Use this for SDMMC IDMA ops
+        #(0x00000400,  63, 2), # ITCM (first 1k removed, to keep address 0 unused)
+        #(0x30040000,  32, 0), # SRAM3.
     ],
+
+    'INSTRUCTION_RAM' : (0x00000400, 63), # ITCM (first 1k removed, to keep address 0 unused)
 
     # avoid a problem in the bootloader by making DTCM first. The DCache init
     # when using SRAM1 as primary memory gets a hard fault in bootloader
@@ -52,10 +48,11 @@ mcu = {
         (0x30000000, 256, 0), # SRAM1, SRAM2
         (0x24000000, 512, 4), # AXI SRAM. Use this for SDMMC IDMA ops
         (0x00000400,  63, 2), # ITCM (first 1k removed, to keep address 0 unused)
-        (0x30040000,  32, 0), # SRAM3.
+        #(0x30040000,  32, 0), # SRAM3.
         (0x38000000,  64, 1), # SRAM4.
     ],
     
+    'ETHERNET_RAM' : (0x30040000, 256, 0), # SRAM3
     'EXPECTED_CLOCK' : 400000000,
 
 	'EXPECTED_CLOCKS' : [
