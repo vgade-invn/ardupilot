ed410ddb1a197774a20091f2ade67b879d336035
Peter Barker <pbarker@barker.dropbear.id.au>
2023-05-14 10:41:34 +1000

    hwdef: more parameter tuning

--- libraries/AP_HAL_ChibiOS/hwdef/Chimera-include/make-configs.sh
+++ libraries/AP_HAL_ChibiOS/hwdef/Chimera-include/make-configs.sh
@@ -3,7 +3,9 @@
 set -e
 set -x
 
-cat tune.parm config.parm DJI.parm >../Chimera-DJI/defaults.parm
-cat tune.parm config.parm Crossfire.parm >../Chimera-XFire-Ana/defaults.parm
+cat tune.parm config.parm DJI.parm esp-012.inc >../Chimera-DJI/defaults.parm
+cat tune.parm config.parm DJI.parm rfd900x.inc >../Chimera-DJI-RFD900x/defaults.parm
 
-cat tune.parm config.parm SharkByte.parm Spektrum.parm >../Chimera-Spektrum-SharkByte/defaults.parm
+cat tune.parm config.parm Crossfire.parm esp-012.inc >../Chimera-XFire-Ana/defaults.parm
+
+cat tune.parm config.parm SharkByte.parm Spektrum.parm esp-012.inc >../Chimera-Spektrum-SharkByte/defaults.parm
